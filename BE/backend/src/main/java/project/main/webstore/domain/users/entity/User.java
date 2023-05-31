71f3d548a3e905ad05a350316bb3387084070222
SigLee <lgh941107@gmail.com>
2023-05-31 15:49:46 +0900

    [feat]: UserEntity 구현 및 enum 추가 UserEntity구현에 필요한 enum 추가

---
+++ BE/backend/src/main/java/project/main/webstore/domain/users/entity/User.java
@@ -0,0 +1,52 @@
+package project.main.webstore.domain.users.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import project.main.webstore.audit.Auditable;
+import project.main.webstore.domain.users.enums.Grade;
+import project.main.webstore.domain.users.enums.ProviderId;
+import project.main.webstore.domain.users.enums.UserRole;
+import project.main.webstore.valueObject.Address;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+import static javax.persistence.EnumType.STRING;
+import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Entity
+public class User extends Auditable {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String nickName;
+    private String password;
+    private String email;
+    private LocalDateTime lastConnectedDate;
+    private int mileage;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(STRING)
+    private Grade grade = Grade.NORMAL;
+    @Enumerated(STRING)
+    private ProviderId providerId = ProviderId.JWT;
+    @Enumerated(STRING)
+    private UserRole userRole = UserRole.CLIENT;
+
+    public User(String nickName, String password, String email, int mileage, Address address, Grade grade, ProviderId providerId, UserRole userRole) {
+        this.nickName = nickName;
+        this.password = password;
+        this.email = email;
+        this.lastConnectedDate = LocalDateTime.now();
+        this.mileage = mileage;
+        this.address = address;
+        this.grade = grade;
+        this.providerId = providerId;
+        this.userRole = userRole;
+    }
+}
