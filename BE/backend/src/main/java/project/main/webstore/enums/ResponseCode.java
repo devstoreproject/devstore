c2ee49a565c337dd8869cb825977b9b5b4eb415f
SigLee <lgh941107@gmail.com>
2023-06-06 23:45:08 +0900

    [feat]: 공통 응답 DtoClass 구현 응답을 일정하게 제공하기 위한 ResponseDto 구현 Http 코드가 아닌 커스텀 코드 이용하기 위한 enum 구현 완료

---
+++ BE/backend/src/main/java/project/main/webstore/enums/ResponseCode.java
@@ -0,0 +1,14 @@
+package project.main.webstore.enums;
+
+import lombok.Getter;
+import project.main.webstore.dto.CustomCode;
+
+@Getter
+public enum ResponseCode implements CustomCode {
+
+    ;
+
+    private String code;
+    private String message;
+
+}
