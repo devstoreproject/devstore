c958abd0b3ee68294b2b623b0cf714950d73740e
SigLee <lgh941107@gmail.com>
2023-06-13 16:21:43 +0900

    [feat]: 이미지 업로드를 위한 메서드 구현 완료

--- BE/backend/src/main/java/project/main/webstore/domain/image/entity/Image.java
+++ BE/backend/src/main/java/project/main/webstore/domain/image/entity/Image.java
@@ -1,6 +1,9 @@
 package project.main.webstore.domain.image.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import project.main.webstore.audit.Auditable;
 
 import javax.persistence.*;
@@ -12,6 +15,7 @@ import static javax.persistence.InheritanceType.*;
 @Getter
 @Inheritance(strategy = JOINED)
 @DiscriminatorColumn(name = "Category")
+@NoArgsConstructor
 public class Image extends Auditable {
     @Id
     @GeneratedValue(strategy = IDENTITY)
@@ -21,8 +25,27 @@ public class Image extends Auditable {
     private String savedTitle;
     private String imagePath;
     private String ext;
+    @Setter
     private int imageOrder;
 
     //썸네일용 하나 추가
     private String thumbnailPath;
+    @Setter
+    private boolean isRepresentative;
+
+    //이미지 순서 및 대표값인지 여부 저장
+    public void setOrderAndRepresentative(int order, boolean isRepresentative){
+        this.imageOrder = order;
+        this.isRepresentative = isRepresentative;
+    }
+
+    public Image(String realTitle, String savedTitle, String imagePath, String ext, String thumbnailPath,int imageOrder, boolean isRepresentative) {
+        this.realTitle = realTitle;
+        this.savedTitle = savedTitle;
+        this.imagePath = imagePath;
+        this.ext = ext;
+        this.thumbnailPath = thumbnailPath;
+        this.imageOrder = imageOrder;
+        this.isRepresentative = isRepresentative;
+    }
 }
