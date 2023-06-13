5c6740865ebae49a708191c2e4a72de6141b4ec7
SigLee <lgh941107@gmail.com>
2023-06-13 16:29:16 +0900

    [feat]: review 이미지 업로드 구현 완료 ReviewImage S3 저장 및 테이블에 구현 완료

--- BE/backend/src/main/java/project/main/webstore/domain/enums/Category.java
+++
@@ -1,9 +0,0 @@
-package project.main.webstore.domain.enums;
-
-import lombok.Getter;
-
-@Getter
-public enum Category {
-    REVIEW,USER_PROFILE,ITEM
-    ;
-}
--- BE/backend/src/main/java/project/main/webstore/domain/review/dto/ReviewPostRequestDto.java
+++ BE/backend/src/main/java/project/main/webstore/domain/review/dto/ReviewPostRequestDto.java
@@ -2,14 +2,7 @@ package project.main.webstore.domain.review.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import project.main.webstore.domain.item.entity.Item;
-import project.main.webstore.domain.users.entity.User;
 
-import javax.persistence.ElementCollection;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -18,5 +11,5 @@ public class ReviewPostRequestDto {
     private Long userId;
     private String comment;
     private int rating;
-    List<String> imagePathList = new ArrayList<>();
+    private List<Boolean> isRepresentativeList;
 }
