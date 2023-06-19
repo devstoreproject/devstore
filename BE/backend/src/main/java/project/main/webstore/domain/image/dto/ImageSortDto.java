07e88be60b666fe153eedd333a4018ba9576717a
SigLee <lgh941107@gmail.com>
2023-06-20 00:27:09 +0900

    [test]: 테스트를 위한 stub 생성을 위한 생성자 추가

--- BE/backend/src/main/java/project/main/webstore/domain/image/dto/ImageSortDto.java
+++ BE/backend/src/main/java/project/main/webstore/domain/image/dto/ImageSortDto.java
@@ -1,8 +1,10 @@
 package project.main.webstore.domain.image.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ImageSortDto {
     private int orderNumber;
     private boolean representative;
--- BE/backend/src/test/java/project/main/webstore/domain/review/stub/ReviewStub.java
+++ BE/backend/src/test/java/project/main/webstore/domain/review/stub/ReviewStub.java
@@ -4,18 +4,21 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import project.main.webstore.domain.image.dto.ImageSortDto;
 import project.main.webstore.domain.item.entity.Item;
 import project.main.webstore.domain.review.dto.ReviewGetResponseDto;
+import project.main.webstore.domain.review.dto.ReviewPostRequestDto;
 import project.main.webstore.domain.review.entity.Review;
 import project.main.webstore.domain.users.entity.User;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ReviewStub {
 
-    public Review createReview(Long userId, Long itemId, Long reviewId){
+    public Review createReview(Long userId, Long itemId, Long reviewId) {
         return Review.stubBuilder()
                 .user(new User(userId))
                 .item(new Item(itemId))
@@ -26,27 +29,49 @@ public class ReviewStub {
                 .stubBuild();
 
     }
-    public ReviewGetResponseDto reviewGetResponseDto(Review review){
+
+    public ReviewGetResponseDto reviewGetResponseDto(Review review) {
         return ReviewGetResponseDto.dtoBuilder().review(review).dtoBuild();
     }
 
     public List<Review> createListReview() {
         List<Review> list = new ArrayList<>();
-        for (Long i = 0L ; i < 10L ; i++){
-            list.add(createReview(i,i,i));
+        for (Long i = 0L; i < 10L; i++) {
+            list.add(createReview(i, i, i));
         }
         return list;
     }
 
-    public Page<Review> createPageReview(int page ,int size) {
+    public Page<Review> createPageReview(int page, int size) {
         PageRequest pageRequest = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "id"));
-        return new PageImpl<>(createListReview(),pageRequest,createListReview().size());
-    }
-    public List<ReviewGetResponseDto> reviewGetResponseListDto(List<Review> reviewList){
-        return reviewList.stream().map(ReviewGetResponseDto::new).collect(Collectors.toList());
-    }
-    public Page<ReviewGetResponseDto> reviewGetResponseDtoPage(int page, int size){
-        return createPageReview(page,size).map(ReviewGetResponseDto::new);
+        return new PageImpl<>(createListReview(), pageRequest, createListReview().size());
     }
 
+    public List<ReviewGetResponseDto> reviewGetResponseListDto(List<Review> reviewList) {
+        return reviewList.stream().map(ReviewGetResponseDto::new).collect(Collectors.toList());
+    }
+
+    public Page<ReviewGetResponseDto> reviewGetResponseDtoPage(int page, int size) {
+        return createPageReview(page, size).map(ReviewGetResponseDto::new);
+    }
+
+    public ReviewPostRequestDto reviewPostRequestDtoNoImage(Long userId) {
+        return ReviewPostRequestDto.stubBuilder().userId(userId).infoList(null).comment("사진이 없는 리뷰").rating(10).stubBuild();
+    }
+
+    public ReviewPostRequestDto reviewPostRequestDto(Long userId) {
+        return ReviewPostRequestDto.stubBuilder().userId(userId).infoList(imageSortListDto()).comment("사진이 없는 리뷰").rating(10).stubBuild();
+    }
+
+
+    public List<ImageSortDto> imageSortListDto() {
+        return Stream.of(
+                imageSortDto(0, false),
+                imageSortDto(1, true)
+        ).collect(Collectors.toList());
+    }
+
+    public ImageSortDto imageSortDto(int order, boolean representative) {
+        return new ImageSortDto(order, representative);
+    }
 }
