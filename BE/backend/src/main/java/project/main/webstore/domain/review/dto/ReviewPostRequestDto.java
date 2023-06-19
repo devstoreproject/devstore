9538f0ad57c81462d62bbd90ac77f5c93393da13
SigLee <lgh941107@gmail.com>
2023-06-20 00:27:09 +0900

    [test]: 테스트를 위한 stub 생성을 위한 생성자 추가

--- BE/backend/src/main/java/project/main/webstore/domain/review/dto/ReviewPostRequestDto.java
+++ BE/backend/src/main/java/project/main/webstore/domain/review/dto/ReviewPostRequestDto.java
@@ -1,17 +1,23 @@
 package project.main.webstore.domain.review.dto;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import project.main.webstore.domain.image.dto.ImageSortDto;
 
 import java.util.List;
 
 @Getter
-@AllArgsConstructor
 public class ReviewPostRequestDto {
     private Long userId;
     private String comment;
     private int rating;
     private List<ImageSortDto> infoList;
 
+    @Builder(builderMethodName = "stubBuilder", buildMethodName = "stubBuild")
+    public ReviewPostRequestDto(Long userId, String comment, int rating, List<ImageSortDto> infoList) {
+        this.userId = userId;
+        this.comment = comment;
+        this.rating = rating;
+        this.infoList = infoList;
+    }
 }
