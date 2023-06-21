3f243185e472e9b99103c11b1119fa00256a1461
SigLee <lgh941107@gmail.com>
2023-06-22 07:34:04 +0900

    [fit]: Image 엔티티 추가 구현 및 매핑 변경

--- BE/backend/src/main/java/project/main/webstore/domain/notice/entity/Notice.java
+++ BE/backend/src/main/java/project/main/webstore/domain/notice/entity/Notice.java
@@ -2,14 +2,15 @@ package project.main.webstore.domain.notice.entity;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import project.main.webstore.audit.Auditable;
+import project.main.webstore.domain.image.entity.NoticeImage;
 import project.main.webstore.domain.users.entity.User;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import static javax.persistence.FetchType.EAGER;
 import static javax.persistence.FetchType.LAZY;
 import static javax.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
@@ -23,14 +24,21 @@ public class Notice extends Auditable {
     @Column(updatable = false)
     private Long id;
 
+    @Setter
+    @OneToMany(mappedBy = "notice",cascade = CascadeType.ALL,orphanRemoval = true)
+    List<NoticeImage> noticeImageList = new ArrayList<>();
+    @Setter
     private String title;
-
     @Lob
+    @Setter
     private String content;
-
-    @ElementCollection(fetch = EAGER)
-    List<String> imagePathList = new ArrayList<>();
+    @Setter
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "USER_ID")
     private User user;
+
+    public void addReviewImage(NoticeImage image){
+        this.noticeImageList.add(image);
+        image.setNotice(this);
+    }
 }
