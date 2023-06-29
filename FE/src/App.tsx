69d5a55bb23f41c3b4a3df3cc1c81218ea51f238
kimyonghui <kyhui1115@gmail.com>
2023-06-29 14:59:34 +0900

    [feat] router 설정 및 페이지 연결

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,5 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import SignIn from 'Pages/KYH/SignIn';
+import SignUp from 'Pages/KYH/SignUp';
+import SleeperAccount from 'Pages/KYH/SleeperAccount';
+import Mypage from 'Pages/KYH/Mypage';
+import ProductList from 'Components/KYH/ProductList/ProductList';
+import ReturnOrExchange from 'Components/KYH/ReturnOrExchange/ReturnOrExchange';
+import ProductInquiry from 'Components/KYH/ProductInquiry/ProductInquiry';
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/sleeperaccount" element={<SleeperAccount />} />
+        <Route path="/mypage/*" element={<Mypage />} />
+        <Route path="/admin/productlist" element={<ProductList />} />
+        <Route path="/admin/returnorexchange" element={<ReturnOrExchange />} />
+        <Route path="/admin/productinquiry" element={<ProductInquiry />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
