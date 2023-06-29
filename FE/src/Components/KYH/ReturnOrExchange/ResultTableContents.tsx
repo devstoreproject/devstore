f59e43004a5f88443ef17b46c27c40b2a24b93b1
kimyonghui <kyhui1115@gmail.com>
2023-06-29 15:05:55 +0900

    [design] 관리자 반품, 교환 페이지 UI 구현

---
+++ FE/src/Components/KYH/ReturnOrExchange/ResultTableContents.tsx
@@ -0,0 +1,21 @@
+export default function ResultTableContents() {
+  return (
+    <div className="flex w-full h-12 border-b rounded-t-lg border-b-gray-400">
+      <span className="flex items-center justify-center w-20">반품</span>
+      <span className="flex items-center justify-center w-20">홍길동</span>
+      <span className="flex items-center justify-center w-36">
+        12315648942315
+      </span>
+      <span className="w-56 text-center truncate leading-12">
+        단순 변심 단순 변심 단순 변심 단순 변심
+      </span>
+      <span className="flex items-center justify-center w-32">2023.06.27</span>
+      <span className="flex items-center justify-center w-40">
+        11111111111111111
+      </span>
+      <span className="flex items-center justify-center w-32">반품승인</span>
+      <span className="flex items-center justify-center w-32">350,000</span>
+      <span className="flex items-center justify-center w-24">5,000</span>
+    </div>
+  );
+}
