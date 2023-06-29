f8770127b9e65764d39c763330f704f0371c18c2
kimyonghui <kyhui1115@gmail.com>
2023-06-29 14:58:03 +0900

    [chore] react-icons 라이브러리 추가, tailwindCSS 설정 추가 등

--- FE/tailwind.config.js
+++ FE/tailwind.config.js
@@ -2,7 +2,33 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      lineHeight: {
+        12: '3rem',
+      },
+      colors: {
+        'neon-green': '#00FF00',
+        'light-gray': '#F7F8FA',
+      },
+      width: {
+        104: '26rem',
+        112: '28rem',
+        120: '30rem',
+        128: '32rem',
+        136: '34rem',
+      },
+      height: {
+        104: '26rem',
+        112: '28rem',
+        120: '30rem',
+        128: '32rem',
+        136: '34rem',
+      },
+      boxShadow: {
+        signBox: '0px 3px 8px rgba(0, 0, 0, 0.24)',
+        btn: 'rgba(0, 0, 0, 0.07) 0px 1px 1px, rgba(0, 0, 0, 0.07) 0px 2px 2px, rgba(0, 0, 0, 0.07) 0px 4px 4px, rgba(0, 0, 0, 0.07) 0px 8px 8px, rgba(0, 0, 0, 0.07) 0px 16px 16px;',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('tailwind-scrollbar-hide')],
 };
