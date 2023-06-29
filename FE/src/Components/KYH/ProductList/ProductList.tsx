040b61913a54dcc57362fff258841258bdc078e5
kimyonghui <kyhui1115@gmail.com>
2023-06-29 15:04:53 +0900

    [design] 관리자 판매상품 페이지 UI 구현

---
+++ FE/src/Components/KYH/ProductList/ProductList.tsx
@@ -0,0 +1,22 @@
+import PaginationContainer from './PaginationContainer';
+import Table from './Table';
+
+export default function ProductList() {
+  return (
+    <div className="flex flex-col w-screen min-h-screen pt-20 pl-48 bg-light-gray">
+      <span className="mb-6 text-xl font-bold">판매 상품</span>
+      <Table />
+      <div className="flex items-center justify-between w-5/6 mt-6">
+        <PaginationContainer />
+        <div>
+          <button className="h-10 px-4 mr-4 border border-gray-500 rounded-3xl">
+            선택 된 상품 삭제하기
+          </button>
+          <button className="h-10 px-4 text-white bg-gray-700 rounded-3xl">
+            상품 추가하기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
