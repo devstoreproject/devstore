4b6f9f25e3aef546ee339bb046b5ec38847e28d6
mya413 <lsm940413@gmail.com>
2023-07-05 14:13:42 +0900

    [design] 공지사항 상세 페이지 UI 구현

---
+++ FE/src/Components/LSM/Notice/NoticeDetail/TabTitle.tsx
@@ -0,0 +1,3 @@
+export default function TabTitle() {
+  return <h3 className="text-lg font-semibold">이벤트</h3>;
+}
--- FE/src/Pages/LSM/Notice/NoticeDetail.tsx
+++ FE/src/Pages/LSM/Notice/NoticeDetail.tsx
@@ -1,3 +1,23 @@
+import Contents from 'Components/LSM/Notice/NoticeDetail/Contents';
+import Date from 'Components/LSM/Notice/NoticeDetail/Date';
+import Icon from 'Components/LSM/Notice/NoticeDetail/Icon';
+import ImageBox from 'Components/LSM/Notice/NoticeDetail/ImageBox';
+import TabTitle from 'Components/LSM/Notice/NoticeDetail/TabTitle';
+import Title from 'Components/LSM/Notice/NoticeDetail/Title';
+
 export default function NoticeDetail() {
-  return <div>NoticeDetail</div>;
+  return (
+    <div className="relative flex flex-col items-center h-screen m-auto mx-32 my-16">
+      <div className="flex flex-col text-center w-250">
+        <TabTitle />
+        <Title />
+        <Date />
+        <div className="m-auto w-250">
+          <ImageBox />
+          <Contents />
+        </div>
+      </div>
+      <Icon />
+    </div>
+  );
 }
