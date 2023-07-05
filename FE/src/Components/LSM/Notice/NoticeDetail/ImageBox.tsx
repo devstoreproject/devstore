3f324a86e726d667476341d1a9abfe5456c595ba
mya413 <lsm940413@gmail.com>
2023-07-05 14:13:42 +0900

    [design] 공지사항 상세 페이지 UI 구현

---
+++ FE/src/Components/LSM/Notice/NoticeDetail/ImageBox.tsx
@@ -0,0 +1,11 @@
+export default function ImageBox() {
+  return (
+    <div className="my-11">
+      <img
+        src="https://images.unsplash.com/photo-1629131726692-1accd0c53ce0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80"
+        alt="notice-image"
+        className="object-cover m-auto w-250 h-120"
+      />
+    </div>
+  );
+}
