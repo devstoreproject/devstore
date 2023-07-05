b78efb389d6a0f87f7c76b4bd991982295ea1318
mya413 <lsm940413@gmail.com>
2023-07-05 15:46:23 +0900

    [feat] 공지사항 메인/상세/등록/수정, 상품 등록/수정 라우터 추가

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -4,8 +4,11 @@ import SignIn from 'Pages/KYH/SignIn';
 import SignUp from 'Pages/KYH/SignUp';
 import SleeperAccount from 'Pages/KYH/SleeperAccount';
 import Mypage from 'Pages/KYH/Mypage';
-
 import Admin from 'Pages/KYH/Admin';
+import NoticeList from 'Pages/LSM/Notice/NoticeList';
+import NoticePost from 'Pages/LSM/Notice/NoticePost';
+import NoticeEdit from 'Pages/LSM/Notice/NoticeEdit';
+import NoticeDetail from 'Pages/LSM/Notice/NoticeDetail';
 
 function App() {
   return (
@@ -16,6 +19,10 @@ function App() {
         <Route path="/sleeperaccount" element={<SleeperAccount />} />
         <Route path="/mypage/*" element={<Mypage />} />
         <Route path="/admin/*" element={<Admin />} />
+        <Route path="/notice" element={<NoticeList />} />
+        <Route path="/notice/post" element={<NoticePost />} />
+        <Route path="/notice/edit" element={<NoticeEdit />} />
+        <Route path="/notice/detail" element={<NoticeDetail />} />
       </Routes>
     </BrowserRouter>
   );
--- FE/src/Pages/KYH/Admin.tsx
+++ FE/src/Pages/KYH/Admin.tsx
@@ -3,6 +3,8 @@ import ProductList from 'Components/KYH/admin/ProductList/ProductList';
 import ReturnOrExchange from 'Components/KYH/admin/ReturnOrExchange/ReturnOrExchange';
 import ProductInquiry from 'Components/KYH/admin/ProductInquiry/ProductInquiry';
 import AdminNav from 'Components/KYH/admin/AdminNav';
+import ProductPost from 'Pages/LSM/Product/ProductPost';
+import ProductEdit from 'Pages/LSM/Product/ProductEdit';
 
 export default function Admin() {
   return (
@@ -11,6 +13,8 @@ export default function Admin() {
       <div className="w-5/6 pt-16 pl-112">
         <Routes>
           <Route path="/productlist" element={<ProductList />} />
+          <Route path="/productlist/post" element={<ProductPost />} />
+          <Route path="/productlist/edit" element={<ProductEdit />} />
           <Route path="/returnorexchange" element={<ReturnOrExchange />} />
           <Route path="/productinquiry" element={<ProductInquiry />} />
         </Routes>
