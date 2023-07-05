0562ee145b62656320a0910eb99724994a41bce8
kimyonghui <kyhui1115@gmail.com>
2023-07-05 14:19:42 +0900

    [design] 관리자 페이지 메뉴 추가

---
+++ FE/src/Components/KYH/admin/AdminNav.tsx
@@ -0,0 +1,14 @@
+import HomeNavigate from './HomeNavigate';
+import MenuNav from './MenuNav';
+
+export default function AdminNav() {
+  return (
+    <div className="fixed flex flex-col h-screen px-6 bg-white w-96 shadow-signBox">
+      <span className="h-20 pt-4 ml-4 text-2xl font-bold">DEV SHOP</span>
+      <div className="w-full h-full border-t border-t-black">
+        <HomeNavigate />
+        <MenuNav />
+      </div>
+    </div>
+  );
+}
