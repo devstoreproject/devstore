753474d34ce1c2fb703996a3de74f6d148078aa4
mya413 <lsm940413@gmail.com>
2023-07-05 16:54:28 +0900

    [refactor] 공지사항 메인페이지 리스트 코드 수정

--- FE/src/Pages/LSM/Notice/NoticeList.tsx
+++ FE/src/Pages/LSM/Notice/NoticeList.tsx
@@ -1,16 +1,13 @@
-import { useLocation } from 'react-router-dom';
 import NoticeTitle from 'Components/LSM/Notice/NoticeTitle';
 import TabList from 'Components/LSM/Notice/NoticeList/TabList';
 import ContentsList from 'Components/LSM/Notice/NoticeList/ContentsList';
 
 export default function NoticeList() {
-  const path: string = useLocation().pathname.slice(8);
-  const pathRender: boolean = path === '';
   return (
-    <div className="mx-32 my-16">
+    <div className="m-auto my-16 max-w-1460">
       <NoticeTitle />
-      {pathRender && <TabList />}
-      {pathRender && <ContentsList />}
+      <TabList />
+      <ContentsList />
     </div>
   );
 }
