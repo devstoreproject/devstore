fafb60564a1a576576574c29658da1dfcb1f07fa
mya413 <lsm940413@gmail.com>
2023-07-05 14:14:34 +0900

    [design] 공지사항 수정 페이지 UI 구현

--- FE/src/Pages/LSM/Notice/NoticeEdit.tsx
+++ FE/src/Pages/LSM/Notice/NoticeEdit.tsx
@@ -1,3 +1,11 @@
+import NoticeForm from 'Components/LSM/Notice/NoticeForm/NoticeForm';
+import NoticeTitle from 'Components/LSM/Notice/NoticeTitle';
+
 export default function NoticeEdit() {
-  return <div>NoticeEdit</div>;
+  return (
+    <div className="mx-auto w-250">
+      <NoticeTitle />
+      <NoticeForm />
+    </div>
+  );
 }
