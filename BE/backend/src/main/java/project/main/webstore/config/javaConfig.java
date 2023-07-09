380c510419dd38f52368841c088f759827a70df1
SigLee <lgh941107@gmail.com>
2023-07-10 04:04:34 +0900

    [feat]: 이메일 전송을 위한 코드 추가

--- BE/backend/src/main/java/project/main/webstore/config/javaConfig.java
+++ BE/backend/src/main/java/project/main/webstore/config/javaConfig.java
@@ -4,6 +4,19 @@ import com.siot.IamportRestClient.IamportClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import project.main.webstore.email.email.EmailSendable;
+import project.main.webstore.email.email.SimpleEmailSendable;
+import project.main.webstore.email.email.TemplateEmailSendable;
+
+import java.util.Properties;
 
 @Configuration
 public class javaConfig {
@@ -11,10 +24,59 @@ public class javaConfig {
     private String apiKey;
     @Value("${iamport.secret-key}")
     private String secretKey;
+    @Value("${mail.smtp.host}")
+    private String host;
+
+    @Value("${mail.smtp.port}")
+    private int port;
+
+    @Value("${mail.smtp.username}")
+    private String username;
+
+    @Value("${mail.smtp.password}")
+    private String password;
+
+    @Value("${mail.smtp.auth}")
+    private String auth;
+
+    @Value("${mail.smtp.starttls.enable}")
+    private String tlsEnable;
 
     @Bean
     public IamportClient iamportClient(){
         return new IamportClient(apiKey, secretKey);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public EmailSendable simpleEmailSendable() {
+        return new SimpleEmailSendable(javaMailSender());
+    }
+
+
+    @Primary
+    @Bean
+    public EmailSendable templateEmailSendable(TemplateEngine templateEngine) {
+        return new TemplateEmailSendable(javaMailSender(), templateEngine, new Context());
+    }
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", auth);
+        props.put("mail.smtp.starttls.enable", tlsEnable);
+
+        return mailSender;
     }
 
 }
---
+++ BE/backend/src/main/java/project/main/webstore/email/email/EmailSendable.java
@@ -0,0 +1,8 @@
+package project.main.webstore.email.email;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface EmailSendable {
+    void send(String[] to, String subject, String message, String templateName) throws InterruptedException;
+}
