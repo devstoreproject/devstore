4e61057a8c30013b11d10556a01b4c1c397a489f
kimyonghui <kyhui1115@gmail.com>
2023-07-11 16:06:15 +0900

    [design] 관리자 메뉴 접기/펼치기 구현

--- FE/src/Pages/KYH/Admin.tsx
+++ FE/src/Pages/KYH/Admin.tsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
+
 import { Outlet } from 'react-router-dom';
 import AdminNav from 'Components/KYH/admin/AdminNav';
 
 export default function Admin() {
+  const [fold, setFold] = useState(false);
+
   return (
-    <div className="flex w-screen min-h-screen bg-light-gray">
-      <AdminNav />
-      <div className="w-5/6 pt-16 pl-104">
+    <div className="flex min-h-screen bg-light-gray">
+      <AdminNav fold={fold} setFold={setFold} />
+      <div
+        className={`w-full pt-16 transition-all duration-200 ease-linear ${
+          fold ? 'pl-40' : 'pl-96'
+        }`}
+      >
         <Outlet />
       </div>
     </div>
