6658fb40d26d103a4236a0811bd911fb7812d3ac
kimyonghui <kyhui1115@gmail.com>
2023-07-12 16:29:29 +0900

    [fix] console 에러 수정

--- FE/src/Components/KHJ/Nav/NavBottom.tsx
+++ FE/src/Components/KHJ/Nav/NavBottom.tsx
@@ -20,20 +20,20 @@ export default function NavBottom() {
       icon: <BiBell size={sizeAll} />,
     },
     {
-      to: '/mypage/alert',
+      to: '/mypage/b',
       icon: <BiSolidCartAlt size={sizeAll} />,
     },
   ];
 
   return (
-    <nav className="flex justify-center items-center relative h-14 bg-white">
+    <nav className="relative flex items-center justify-center bg-white h-14">
       <button className="absolute left-5 top-2/4 -translate-y-2/4">
         <RxHamburgerMenu size={24} />
       </button>
-      <h1 className="whitespace-nowrap text-xl font-bold text-light-black">
+      <h1 className="text-xl font-bold whitespace-nowrap text-light-black">
         DEV SHOP
       </h1>
-      <ul className="flex absolute top-2/4 right-5 -translate-y-2/4">
+      <ul className="absolute flex top-2/4 right-5 -translate-y-2/4">
         {linkTo.map((link) => (
           <li className="ml-6" key={link.to}>
             <Link to={link.to}>{link.icon}</Link>
