f40404e7cfa51bff573db0cf58b47a450ef00529
SigLee <lgh941107@gmail.com>
2023-07-15 10:19:44 +0900

    [feat]: QnaController 스웨거 구현

--- BE/backend/src/main/java/project/main/webstore/domain/qna/controller/QnaController.java
+++ BE/backend/src/main/java/project/main/webstore/domain/qna/controller/QnaController.java
@@ -1,5 +1,7 @@
 package project.main.webstore.domain.qna.controller;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -27,7 +29,8 @@ public class QnaController {
     private final QnaMapper mapper;
 
     @GetMapping("/item/{itemId}")
-    public ResponseEntity getQnaByItemId(Pageable pageable, @PathVariable Long itemId) {
+    @ApiResponse(responseCode = "200", description = "상품에해당하는 Qna 조회")
+    public ResponseEntity<ResponseDto<Page<QuestionDto>>> getQnaByItemId(Pageable pageable, @PathVariable Long itemId) {
         Page<Question> findQna = getService.findQnaByItemId(pageable, itemId);
         Page<QuestionDto> response = mapper.toResponsePage(findQna);
         var responseDto = ResponseDto.<Page<QuestionDto>>builder().data(response).customCode(ResponseCode.OK).build();
@@ -35,7 +38,8 @@ public class QnaController {
     }
 
     @GetMapping("/user/{userId}")
-    public ResponseEntity getQnaByUserId(Pageable pageable, @PathVariable Long userId) {
+    @ApiResponse(responseCode = "200", description = "사용자가 작성한 Qna 페이지")
+    public ResponseEntity<ResponseDto<Page<QuestionDto>>> getQnaByUserId(Pageable pageable, @PathVariable Long userId) {
         Page<Question> findQna = getService.findQnaByUserId(pageable, userId);
         Page<QuestionDto> response = mapper.toResponsePage(findQna);
         var responseDto = ResponseDto.<Page<QuestionDto>>builder().data(response).customCode(ResponseCode.OK).build();
@@ -43,24 +47,26 @@ public class QnaController {
     }
 
     @GetMapping("/{questionId}")
-    public ResponseEntity getQna(Long userId, @PathVariable Long questionId){
+    @ApiResponse(responseCode = "200", description = "QnA 단건 조회")
+    public ResponseEntity<ResponseDto<QuestionDto>> getQna(Long userId, @PathVariable Long questionId){
         Question result = getService.findQuestion(userId, questionId);
         QuestionDto response = mapper.toResponseDto(result);
         var responseDto = ResponseDto.<QuestionDto>builder().data(response).customCode(ResponseCode.OK).build();
         return ResponseEntity.ok(responseDto);
     }
 
-    //시큐리티 적용 이후 변경예정
     @GetMapping("/admin")
-    public ResponseEntity getQnaByStatus(@RequestParam Long userId, Pageable pageable) {
+    @Tag(name = "관리자 API")
+    @ApiResponse(responseCode = "200", description = "관리자가 Qna 조회(질문 게시 상태인 것들)")
+    public ResponseEntity<ResponseDto<Page<QuestionDto>>> getQnaByStatus(@RequestParam Long userId, Pageable pageable) {
         Page<Question> result = getService.findQuestionByStatus(userId, pageable);
         Page<QuestionDto> response = mapper.toResponsePage(result);
         var responseDto = ResponseDto.<Page<QuestionDto>>builder().data(response).customCode(ResponseCode.OK).build();
         return ResponseEntity.ok(responseDto);
     }
-
     @PostMapping("/item/{itemId}")
-    public ResponseEntity postQuestion(@PathVariable Long itemId, @RequestBody QuestionPostRequestDto postDto) {
+    @ApiResponse(responseCode = "201", description = "질문 등록")
+    public ResponseEntity<ResponseDto<QuestionDto>> postQuestion(@PathVariable Long itemId, @RequestBody QuestionPostRequestDto postDto) {
         Question request = mapper.toEntity(postDto,itemId);
         Question result = service.postQuestion(request);
         QuestionDto response = mapper.toResponseDto(result);
@@ -70,7 +76,8 @@ public class QnaController {
     }
 
     @PatchMapping("/item/{itemId}/{questionId}")
-    public ResponseEntity patchQuestion(@PathVariable Long questionId,
+    @ApiResponse(responseCode = "200", description = "질문 수정")
+    public ResponseEntity<ResponseDto<QuestionDto>> patchQuestion(@PathVariable Long questionId,
                                         @PathVariable Long itemId,
                                         @RequestBody QuestionPatchDto patchDto) {
         Question request = mapper.toEntity(patchDto, questionId, itemId);
@@ -82,13 +89,15 @@ public class QnaController {
     }
 
     @DeleteMapping("/qna/{questionId}")
+    @ApiResponse(responseCode = "204", description = "질문 삭제")
     public ResponseEntity deleteQuestion(@PathVariable Long questionId, Long userId){
         service.deleteQuestion(questionId,userId);
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/qna/{questionId}/answer")
-    public ResponseEntity postAnswer(@PathVariable Long questionId,@RequestBody AnswerPostRequestDto postDto){
+    @ApiResponse(responseCode = "201", description = "답변 등록")
+    public ResponseEntity<ResponseDto<AnswerDto>> postAnswer(@PathVariable Long questionId,@RequestBody AnswerPostRequestDto postDto){
         Answer request = mapper.toEntity(postDto, questionId);
         Answer result = service.postAnswer(request);
         AnswerDto response = mapper.toResponseDto(result);
@@ -98,6 +107,7 @@ public class QnaController {
     }
 
     @DeleteMapping("/qna/{questionId}/answer/{answerId}")
+    @ApiResponse(responseCode = "204", description = "답변 삭제")
     public ResponseEntity deleteAnswer(@PathVariable Long answerId,@RequestParam Long userId){
         service.deleteAnswer(userId, answerId);
         return ResponseEntity.noContent().build();
--- BE/backend/src/main/java/project/main/webstore/domain/qna/dto/AnswerPostRequestDto.java
+++ BE/backend/src/main/java/project/main/webstore/domain/qna/dto/AnswerPostRequestDto.java
@@ -1,13 +1,18 @@
 package project.main.webstore.domain.qna.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Schema(description = "질문 등록")
 public class AnswerPostRequestDto {
+    @Schema(description = "질문 본문")
     private String comment;
+    @Schema(description = "비밀글 여부")
     private boolean isSecret;
+    @Schema(description = "사용자 식별자")
     private Long userId;
 
 }
