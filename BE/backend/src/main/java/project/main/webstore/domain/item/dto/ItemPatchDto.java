93f4b91d315fec9dfca3654ddae3af173d1c0f1b
SigLee <lgh941107@gmail.com>
2023-07-16 15:50:28 +0900

    [feat]: Item 관련 스키마 어노테이션 설정

--- BE/backend/src/main/java/project/main/webstore/domain/item/dto/ItemPatchDto.java
+++ BE/backend/src/main/java/project/main/webstore/domain/item/dto/ItemPatchDto.java
@@ -1,5 +1,6 @@
 package project.main.webstore.domain.item.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,9 +13,12 @@ import java.util.List;
 
 @Getter
 @Setter
+@Schema(description = "상품 수정 스키마")
 public class ItemPatchDto {
+    @Schema(example = "COMPUTER",allowableValues = {"COMPUTER", "MONITOR", "MOUSE", "HEADSET", "CHAIR", "DESK"})
     @Pattern(regexp = "^[가-힣a-zA-Z0-9\\s]*$")
     private Category category;
+    @Schema(example = "노트북 맥북")
     @Pattern(regexp = "^[가-힣a-zA-Z0-9\\s]*$")
     private String itemName;
     @Pattern(regexp = "^[가-힣a-zA-Z\\d`~!@#$%^&*()-_=+\\s]*$")
@@ -25,7 +29,9 @@ public class ItemPatchDto {
 
 
     //이미지 삭제 로직
+    @Schema(implementation = Long.class, description = "삭제할 이미지의 ID 리스트")
     private List<Long> deleteImageId;
+    @Schema(implementation = ImageSortDto.class,description = "이미지 정렬 및 대표사진 여부 정보 리스트")
     private List<ImageSortDto> imageSortAndRepresentativeInfo;
 
     @Builder
