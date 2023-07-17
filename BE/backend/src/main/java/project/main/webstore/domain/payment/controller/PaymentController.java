67fba8834fcee227da71619f62f3de3059c2e336
SigLee <lgh941107@gmail.com>
2023-07-17 16:59:52 +0900

    [feat]: 결제 검증 컨트롤러 스웨거 추가

--- BE/backend/src/main/java/project/main/webstore/domain/payment/controller/PaymentController.java
+++ BE/backend/src/main/java/project/main/webstore/domain/payment/controller/PaymentController.java
@@ -3,12 +3,16 @@ package project.main.webstore.domain.payment.controller;
 import com.siot.IamportRestClient.request.PrepareData;
 import com.siot.IamportRestClient.response.IamportResponse;
 import com.siot.IamportRestClient.response.Prepare;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import project.main.webstore.domain.payment.service.PaymentService;
 import project.main.webstore.dto.ResponseDto;
 import project.main.webstore.enums.ResponseCode;
+import project.main.webstore.utils.CheckLoginUser;
 import project.main.webstore.utils.UriCreator;
 
 import java.net.URI;
@@ -16,6 +20,7 @@ import java.net.URI;
 @RestController
 @RequestMapping("api/payment")
 @RequiredArgsConstructor
+@Tag(name = "주문 API",description = "결제 검증 API")
 public class PaymentController {
     private final PaymentService paymentService;
 
@@ -27,6 +32,7 @@ public class PaymentController {
 //    }
 
     @PostMapping("/prepare")
+    @ApiResponse(responseCode = "201",description = "결제 정보 사전 등록\n 사전등록된 정보를 추 후 검증할 때 사용하기 위한 저장")
     public ResponseEntity postPrepare(@RequestBody PrepareData prepareData) {
         PrepareData resultPrepare = paymentService.postPrepare(prepareData);
         var responseDto = ResponseDto.<PrepareData>builder().data(resultPrepare).customCode(ResponseCode.CREATED);
@@ -35,6 +41,7 @@ public class PaymentController {
     }
 
     @GetMapping("/pre-valid/{orderNumber}")
+    @ApiResponse(responseCode = "200",description = "결제 정보 사전 검증")
     public ResponseEntity getPrepare(@PathVariable String orderNumber, @RequestParam int price){
         IamportResponse<Prepare> prepare = paymentService.getPrepare(orderNumber, price);
         var responseDto = ResponseDto.<IamportResponse<Prepare>>builder().data(prepare).customCode(ResponseCode.OK).build();
@@ -42,10 +49,13 @@ public class PaymentController {
     }
 
     @GetMapping("/post-vlaid/{orderNumber}")
+    @ApiResponse(responseCode = "200",description = "결제 정보 사후 검증")
     public ResponseEntity validPayment(@ModelAttribute PrepareData prepareData,
                                        @RequestParam String iamId,
                                        @PathVariable String orderNumber,
-                                       @RequestParam long userId){  //로그인 성공 시 로직 변경 필요
+                                       @RequestParam Long userId,
+                                       @AuthenticationPrincipal Object principal){  //로그인 성공 시 로직 변경 필요
+        CheckLoginUser.validUserSame(principal,userId);
         String s = paymentService.validatePayment(prepareData, iamId, orderNumber, userId);
         var responseDto = ResponseDto.builder().data(s).build();
         return ResponseEntity.ok(responseDto);
