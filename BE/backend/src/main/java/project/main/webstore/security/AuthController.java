48edc61e24ad0f44b86f536eadb7b855db504463
SigLee <lgh941107@gmail.com>
2023-07-17 14:03:10 +0900

    [fit]: url 통일 /api/auth/logout -> /api/log-out

--- BE/backend/src/main/java/project/main/webstore/security/AuthController.java
+++ BE/backend/src/main/java/project/main/webstore/security/AuthController.java
@@ -1,5 +1,7 @@
 package project.main.webstore.security;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,15 +15,17 @@ import project.main.webstore.security.jwt.utils.TransMessageUtils;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
-@RequestMapping("/api/auth")
+@RequestMapping("/api")
 @RestController
 @AllArgsConstructor
+@Tag(name = "로그인 관련 API", description = "로그아웃, 토큰 재발급용")
 public class AuthController {
     private final RedisUtils redisUtils;
     private final JwtTokenizer tokenizer;
     private final TransMessageUtils transUtils;
 
-    @GetMapping("/refresh")
+    @GetMapping("/auth/refresh")
+    @ApiResponse(responseCode = "200",description = "엑세스 시간 만료 시 사용되는 API\n Cookie 에 있는 리프레쉬 토큰을 통해 재발급")
     public ResponseEntity getAccessToken(@CookieValue("refreshToken") String refresh, HttpServletResponse response) {
         if (refresh == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).header("Location", "/api/login").body(ErrorResponse.of(HttpStatus.UNAUTHORIZED));
@@ -43,7 +47,8 @@ public class AuthController {
 
     }
 
-    @DeleteMapping("/logout")
+    @DeleteMapping("/log-out")
+    @ApiResponse(responseCode = "204",description = "로그 아웃 시 서버에 저장된 리프레쉬 토큰 정보 삭제")
     public ResponseEntity logout(@CookieValue("refreshToken") String refresh) {
         Object byKey = redisUtils.findByKey(refresh);
         if (byKey != null) {
