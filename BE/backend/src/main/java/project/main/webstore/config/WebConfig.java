572297945a2a1cc9c86b2eac7afb4cc71a7cf80f
SigLee <lgh941107@gmail.com>
2023-07-18 15:00:34 +0900

    [feat]: 스웨거를 위한 메시지컨버터 추가 스웨거 멀티파트폼데이터 octet-stream 형식 JSON 변경을 위한 컨버터 추가

--- BE/backend/src/main/java/project/main/webstore/config/WebConfig.java
+++ BE/backend/src/main/java/project/main/webstore/config/WebConfig.java
@@ -1,14 +1,26 @@
 package project.main.webstore.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import project.main.webstore.converter.EnumConverter;
+
+import java.util.Arrays;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new EnumConverter());
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter octetStreamJsonConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setSupportedMediaTypes(Arrays.asList(new MediaType("application", "octet-stream")));
+        return converter;
     }
 }
