05b343ea84bd3a9747103197e6822c877b04b79e
SigLee <lgh941107@gmail.com>
2023-07-18 15:39:37 +0900

    [feat]: 스웨거 로그인 구현 이전 임시 로그인 파일 구현

--- BE/backend/src/main/java/project/main/webstore/security/AuthController.java
+++ BE/backend/src/main/java/project/main/webstore/security/AuthController.java
@@ -4,16 +4,24 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import project.main.webstore.domain.users.entity.User;
+import project.main.webstore.domain.users.service.UserService;
+import project.main.webstore.dto.ResponseDto;
+import project.main.webstore.enums.ResponseCode;
 import project.main.webstore.exception.ErrorResponse;
 import project.main.webstore.redis.RedisUtils;
+import project.main.webstore.security.dto.LoginDto;
+import project.main.webstore.security.dto.LoginResponseDto;
 import project.main.webstore.security.dto.UserInfoDto;
 import project.main.webstore.security.jwt.utils.JwtTokenizer;
 import project.main.webstore.security.jwt.utils.TransMessageUtils;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RequestMapping("/api")
 @RestController
@@ -23,7 +31,7 @@ public class AuthController {
     private final RedisUtils redisUtils;
     private final JwtTokenizer tokenizer;
     private final TransMessageUtils transUtils;
-
+    private final UserService userService;
     @GetMapping("/auth/refresh")
     @ApiResponse(responseCode = "200",description = "엑세스 시간 만료 시 사용되는 API\n Cookie 에 있는 리프레쉬 토큰을 통해 재발급")
     public ResponseEntity getAccessToken(@CookieValue("refreshToken") String refresh, HttpServletResponse response) {
@@ -56,5 +64,23 @@ public class AuthController {
         }
 
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(path = "/tmp/login",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponse(responseCode = "200",description = "이 것은 임시 다른것과 모든것이 동일하지만 URL이 /api/login 으로 입력하셔야합니다.")
+    public ResponseEntity<ResponseDto<LoginResponseDto>> tmpLogin(@RequestBody LoginDto login, HttpServletResponse response) throws IOException {
+        User user = userService.tmpLogin(login);
+        LoginResponseDto loginResponseDto = new LoginResponseDto(user);
+        UserInfoDto userInfo = new UserInfoDto(user);
+        String accessToken = tokenizer.delegateAccessToken(userInfo);
+        String refreshToken = tokenizer.delegateRefreshToken(userInfo);
+        Cookie cookie = transUtils.createCookie(refreshToken);
+        response.setHeader("Authorization", "Bearer" + accessToken);
+        response.setHeader("Refresh", refreshToken);
+        response.addCookie(cookie);
+
+        ResponseDto<LoginResponseDto> build = ResponseDto.<LoginResponseDto>builder().data(loginResponseDto).customCode(ResponseCode.OK).build();
+        return ResponseEntity.ok(build);
+
     }
 }
