1beebeafaf6cacef0c7b04db5994e0036904ad6c
zoe <zoe_e66@naver.com>
2023-07-19 18:05:56 +0900

    [feat]feat/BE/orderEntity
    - pull 충돌 image 엔티티 merge update

--- BE/backend/src/main/java/project/main/webstore/domain/image/dto/ImageSortPatchInfo.java
+++ BE/backend/src/main/java/project/main/webstore/domain/image/dto/ImageSortPatchInfo.java
@@ -1,11 +1,13 @@
 package project.main.webstore.domain.image.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class ImageSortPatchInfo {
+    @Schema(description = "이미지 식별자 \n post 시 null 값으로 던짐")
     private Long imageId;
     private int orderNumber;
     private boolean representative;
