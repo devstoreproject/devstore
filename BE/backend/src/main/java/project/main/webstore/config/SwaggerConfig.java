7f5286484ba3420025876a99d697460b174112fb
zoe <zoe_e66@naver.com>
2023-07-19 18:07:55 +0900

    [feat]feat/BE/orderEntity
    - pull 충돌 config 엔티티 merge update

--- BE/backend/src/main/java/project/main/webstore/config/SwaggerConfig.java
+++ BE/backend/src/main/java/project/main/webstore/config/SwaggerConfig.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,21 +26,27 @@ import java.util.Map;
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
+        SecurityScheme bearAuth = new SecurityScheme().name("Bearer Authentication").type(SecurityScheme.Type.HTTP).bearerFormat("JWT").scheme("Bearer");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearAuth");
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+                .components(new Components().addSecuritySchemes("bearAuth", bearAuth))
+                .addSecurityItem(securityRequirement)
+                .info(apiInfo())
+                ;
     }
 
     private Info apiInfo() {
         return new Info()
                 .title("Springdoc 테스트")
-                .description("Springdoc을 사용한 Swagger UI 테스트")
+                .description("DEV-STORE BackEnd-API")
                 .version("1.0.0")
                 .contact(new Contact().email("siglee2247@gmail.com")
                         .name("dev-store BE developer")
-                        .extensions(Map.of("email","boj480@gmail.com")))
+                        .extensions(Map.of("email", "boj480@gmail.com")))
                 .summary("쇼핑몰 개발을 위한 API")
-                .termsOfService("BackEndOfDevStore");
+                .termsOfService("BackEndOfDevStore")
+                ;
     }
+
 }
 
