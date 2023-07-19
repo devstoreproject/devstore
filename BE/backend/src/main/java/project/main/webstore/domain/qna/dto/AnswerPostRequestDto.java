7f2ae46c3ea2d20aaaf53df1e234e08630a2b879
zoe <zoe_e66@naver.com>
2023-07-19 18:08:04 +0900

    [feat]feat/BE/orderEntity
    - pull 충돌 qna 엔티티 merge update

--- BE/backend/src/main/java/project/main/webstore/domain/qna/dto/AnswerPostRequestDto.java
+++ BE/backend/src/main/java/project/main/webstore/domain/qna/dto/AnswerPostRequestDto.java
@@ -3,16 +3,18 @@ package project.main.webstore.domain.qna.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @Schema(description = "질문 등록")
 public class AnswerPostRequestDto {
-    @Schema(description = "질문 본문")
+    @Schema(description = "질문 본문",example = "답변이 등록되는 부분입니다.")
     private String comment;
     @Schema(description = "비밀글 여부")
     private boolean isSecret;
-    @Schema(description = "사용자 식별자")
+    @Schema(description = "사용자 식별자",example = "2")
     private Long userId;
 
 }
