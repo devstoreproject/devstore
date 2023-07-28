36867f91d2ba59ab154c504e5860b4d045707210
kimyonghui <kyhui1115@gmail.com>
2023-07-28 22:14:03 +0900

    [feat] 로그인 시 로그인, 회원가입 페이지 접근제한 기능 구현

--- FE/src/Pages/KYH/SignIn.tsx
+++ FE/src/Pages/KYH/SignIn.tsx
@@ -1,6 +1,17 @@
 import SignInForm from 'Components/KYH/SignIn/SignInForm';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function SignIn() {
+  const navigate = useNavigate();
+  const isLogin = localStorage.getItem('authorization');
+
+  useEffect(() => {
+    if (isLogin !== null) {
+      navigate('/');
+    }
+  }, [isLogin]);
+
   return (
     <div className="flex justify-center h-screen bg-light-gray">
       <SignInForm />
