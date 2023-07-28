256df187d5b11de32a927aebc334272e43788cde
kimyonghui <kyhui1115@gmail.com>
2023-07-28 22:08:56 +0900

    [design] 반품 교환 신청 확인 페이지 디자인 및 배치방식 수정

--- FE/src/Components/KYH/admin/ReturnOrExchange/ResultTableContents.tsx
+++ FE/src/Components/KYH/admin/ReturnOrExchange/ResultTableContents.tsx
@@ -1,21 +1,18 @@
 export default function ResultTableContents() {
   return (
-    <div className="flex w-full h-12 border-b rounded-t-lg border-b-gray-400">
-      <span className="flex items-center justify-center w-20">반품</span>
-      <span className="flex items-center justify-center w-20">홍길동</span>
-      <span className="flex items-center justify-center w-36">
-        12315648942315
-      </span>
-      <span className="ml-4 text-center truncate w-52 leading-12">
+    <div className="flex items-center justify-between h-12 px-6 border-b rounded-t-lg border-b-gray-400">
+      <span>1</span>
+      <span className="w-12 text-center">반품</span>
+      <span className="w-16 text-center">홍길동</span>
+      <span className="text-center w-36">12315648942315</span>
+      <span className="w-48 text-center truncate">
         단순 변심 단순 변심 단순 변심 단순 변심
       </span>
-      <span className="flex items-center justify-center w-32">2023.06.27</span>
-      <span className="flex items-center justify-center w-40">
-        11111111111111111
-      </span>
-      <span className="flex items-center justify-center w-32">반품승인</span>
-      <span className="flex items-center justify-center w-32">350,000</span>
-      <span className="flex items-center justify-center w-24">5,000</span>
+      <span className="w-32 text-center">2023.06.27</span>
+      <span className="w-40 text-center">11111111111111111</span>
+      <span className="w-20 text-center">반품승인</span>
+      <span className="w-24 text-center">350,000</span>
+      <span className="w-16 text-center">5,000</span>
     </div>
   );
 }
