f7f036095650352d73b128605484666016ed161c
kimyonghui <kyhui1115@gmail.com>
2023-07-28 22:09:51 +0900

    [feat] 로그인 기능 및 토큰, 리프레쉬 토큰 저장 기능 구현

--- FE/src/Components/KYH/SignIn/EmailAndPWContainer.tsx
+++ FE/src/Components/KYH/SignIn/EmailAndPWContainer.tsx
@@ -1,11 +1,37 @@
-export default function EmailAndPWContainer() {
+interface EmailAndPWContainerProps {
+  email: string;
+  password: string;
+  setEmail: React.Dispatch<React.SetStateAction<string>>;
+  setPassword: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function EmailAndPWContainer({
+  email,
+  setEmail,
+  password,
+  setPassword,
+}: EmailAndPWContainerProps) {
   return (
     <div className="mt-4">
-      <input type="email" className="w-full h-10 border border-black" />
+      <input
+        type="email"
+        className="w-full h-10 border border-black"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        value={email}
+      />
       <span className="mt-1 ml-2 text-xs font-bold text-red-500">
         올바른 형식의 이메일을 입력해주세요
       </span>
-      <input type="password" className="w-full h-10 mt-2 border border-black" />
+      <input
+        type="password"
+        className="w-full h-10 mt-2 border border-black"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        value={password}
+      />
       <span className="mt-1 ml-2 text-xs font-bold text-red-500">
         비밀번호가 일치하지 않습니다
       </span>
--- FE/src/Components/KYH/SignIn/SignInForm.tsx
+++ FE/src/Components/KYH/SignIn/SignInForm.tsx
@@ -3,24 +3,56 @@ import EmailAndPWContainer from './EmailAndPWContainer';
 import KeepLoginContainer from './KeepLoginContainer';
 import SearchPasswordContainer from './SearchPasswordContainer';
 import OneClickLogInContainer from './OneClickLogInContainer';
+import { useState } from 'react';
+import api from 'api';
 
 export default function SignInForm() {
   const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const navigateHandler = (e: React.MouseEvent) => {
     e.preventDefault();
     navigate('/signup');
   };
 
+  const submitHandler = (e: React.FormEvent): void => {
+    e.preventDefault();
+    api
+      .post('/api/login', {
+        username: email,
+        password,
+      })
+      .then((res) => {
+        localStorage.setItem('authorization', res.headers.authorization);
+        localStorage.setItem('refresh', res.headers.refresh);
+        navigate('/');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
-    <form className="flex flex-col items-center p-5 mt-40 bg-white rounded-xl w-80 h-120 shadow-signBox">
+    <form
+      className="flex flex-col items-center p-5 mt-40 bg-white rounded-xl w-80 h-120 shadow-signBox"
+      onSubmit={submitHandler}
+    >
       <span className="mt-2 text-2xl font-bold">DEV SHOP</span>
-      <EmailAndPWContainer />
+      <EmailAndPWContainer
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+      />
       <div className="flex justify-between mt-2">
         <KeepLoginContainer />
         <SearchPasswordContainer />
       </div>
-      <button className="mt-4 w-full text-white bg-black h-11 rounded-xl shadow-btn active:shadow-none active:ml-0.5 active:mt-4.5 duration-100">
+      <button
+        className="mt-4 w-full text-white bg-black h-11 rounded-xl shadow-btn active:shadow-none active:ml-0.5 active:mt-4.5 duration-100"
+        type="submit"
+      >
         로그인
       </button>
       <button
