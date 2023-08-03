3c1befeb99e4a9630621aa8e0f2230083836797f
cyweyo <cyw7715@gmail.com>
2023-08-03 18:49:03 +0900

    [design]  리뷰 관리 페이지 최종 컴포넌트 구성 완료

--- FE/src/Pages/CYW/ReviewManagement.tsx
+++ FE/src/Pages/CYW/ReviewManagement.tsx
@@ -1,17 +1,17 @@
 import AllReviewManagement from 'Components/CYW/RewiewManagement/AllReviewManagement';
-import RegisteredReviewManagement from 'Components/CYW/RewiewManagement/RegisteredReviewManagement';
+import BestReviewManagement from 'Components/CYW/RewiewManagement/BestReviewManagement';
 import { BiSolidCommentCheck } from 'react-icons/bi';
 
 export default function ReviewManagement() {
   return (
-    <div className="flex flex-col lg:w-full bg-light-gray">
+    <div className="flex flex-col bg-light-gray">
       <div className="flex items-center">
         <BiSolidCommentCheck size={30} />
         <h1 className="pl-2 font-bold text-2xl">리뷰</h1>
       </div>
       <div className="pt-10">
-        <h2 className="font-bold text-xl pb-5">등록된 리뷰 목록</h2>
-        <RegisteredReviewManagement />
+        <h2 className="font-bold text-xl pb-5">베스트 리뷰 목록</h2>
+        <BestReviewManagement />
       </div>
       <div className="pt-10">
         <h2 className="font-bold text-xl pb-5">전체 리뷰 목록</h2>
