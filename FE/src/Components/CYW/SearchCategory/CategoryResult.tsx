ea36a2223a24a11f2c43433ae361c75b9fb94160
cyweyo <cyw7715@gmail.com>
2023-08-03 18:46:57 +0900

    [design] 카테고리 상품 검색 페이지 UI구현 완성

---
+++ FE/src/Components/CYW/SearchCategory/CategoryResult.tsx
@@ -0,0 +1,16 @@
+import ResultItem from './ResultItem';
+
+export default function CategoryResult() {
+  return (
+    <div className="flex flex-wrap justify-between pt-16 pb-16 w-365">
+      <ResultItem />
+      <ResultItem />
+      <ResultItem />
+      <ResultItem />
+      <ResultItem />
+      <ResultItem />
+      <ResultItem />
+      <ResultItem />
+    </div>
+  );
+}
