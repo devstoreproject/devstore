f86826ed6f5a2e7185189735ec40e435ca4da347
zoe <zoe_e66@naver.com>
2023-08-03 16:42:27 +0900

    [feat]feat/BE/orderEntity
    - order entity 수정
    - cart, item, shippingInfo 도메인 연결, 수정 작업
    - Id값으로 필요 데이터 사용

--- BE/backend/src/main/java/project/main/webstore/domain/order/dto/OrderResponseDto.java
+++ BE/backend/src/main/java/project/main/webstore/domain/order/dto/OrderResponseDto.java
@@ -4,42 +4,47 @@ import lombok.*;
 import org.checkerframework.common.value.qual.ArrayLen;
 import project.main.webstore.domain.item.entity.Item;
 import project.main.webstore.domain.order.entity.Orders;
+import project.main.webstore.domain.order.enums.OrdersStatus;
 import project.main.webstore.domain.orderHistory.enums.OrderStatus;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
-@Builder
 @NoArgsConstructor
-@AllArgsConstructor
-public class OrderResponseDto {
-    // OrderOrderForm
+public class OrderResponseDto { // 주문서 총 정보
+    private Long orderId;
     private String orderNumber;
-    private String recipient;
-    private String email;
-    private String mobileNumber;
-    private String homeNumber;
-    private String zipCode;
-    private String addressSimple;
-    private String addressDetail;
+
+    private ShippingAddressInfoDto addressInfo;
+    private UserOrderDto userInfo;
+
+    private List<OrderItemResponseDto> orderItemList;
+
+    private int totalPrice;
+    private int discountedPrice;
+    private int deliveryPrice;
+
+    private OrdersStatus ordersStatus;
     private String message;
 
-
-    private Long orderId;
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
-//    private List<OrderFormResponseDto> orderFormList;
-    private List<OrderItemResponseDto> orderItemList;
-    private int totalPrice;
-    private OrderStatus ordersStatus;
 
-    public void OrderResponseDto(Item item) {
-        this.totalPrice = item.getTotalPrice();
-    }
-
-    public OrderResponseDto(OrderStatus ordersStatus) {
-        this.ordersStatus = ordersStatus;
+    public OrderResponseDto(Orders order) {
+        this.orderId = order.getOrderId();
+        this.orderNumber = order.getOrderNumber();
+        this.addressInfo = new ShippingAddressInfoDto(order.getInfo());
+        this.userInfo = new UserOrderDto(order.getUser());
+        this.orderItemList = order.getCart().getCartItemList().stream().map(OrderItemResponseDto::new).collect(Collectors.toList());
+        this.totalPrice = order.getTotalPrice();
+        this.discountedPrice = order.getDiscountPrice();
+        this.deliveryPrice = order.getDeliveryPrice();
+        this.ordersStatus = order.getOrdersStatus();
+        this.message = order.getMessage();
+        this.createdAt = order.getCreatedAt();
+        this.modifiedAt = order.getModifiedAt();
     }
 }
--- BE/backend/src/main/java/project/main/webstore/domain/users/entity/ShippingInfo.java
+++ BE/backend/src/main/java/project/main/webstore/domain/users/entity/ShippingInfo.java
@@ -10,9 +10,9 @@ import static javax.persistence.FetchType.LAZY;
 import static javax.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
+@Builder
 @Entity
 @Getter
-@Setter
 @Table(name = "SHIPPING_INFO")
 @NoArgsConstructor(access = PROTECTED)
 @AllArgsConstructor
@@ -24,26 +24,27 @@ public class ShippingInfo {
     @Setter
     @Column(updatable = false)
     private String recipient; // 배송받는사람
-    @Setter
-    @Column(insertable = false, updatable = false)
-    private String email; // 이메일
-    @Setter
-    @Column(insertable = false, updatable = false)
-    private String zipCode;
-    @Setter
-    @Column(updatable = false)
-    private String addressSimple;
-    @Setter
-    @Column(updatable = false)
-    private String addressDetail;
+//    @Setter
+//    @Column(updatable = false)
+//    private String email; // 이메일
+//    @Setter
+//    @Column(updatable = false)
+//    private String zipCode;
+//    @Setter
+//    @Column(updatable = false)
+//    private String addressSimple;
+//    @Setter
+//    @Column(updatable = false)
+//    private String addressDetail;
     @Embedded
     private Address address;
 
     @Setter
-    @ManyToOne(fetch = LAZY, cascade = CascadeType.ALL) // 배송지 여러개 등록 가능
+    @ManyToOne(fetch = LAZY) // 배송지 여러개 등록 가능
     @JoinColumn(name = "USER_ID")
     private User user;
 
+    @Setter
     @OneToOne(fetch = LAZY)
     @JoinColumn(name = "ORDER_ID")
     private Orders order;
