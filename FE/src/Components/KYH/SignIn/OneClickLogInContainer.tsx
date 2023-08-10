501576d37708e695e4828af986b31776a4ce097d
kimyonghui <kyhui1115@gmail.com>
2023-08-10 16:07:57 +0900

    [design] 로그인 페이지 디자인 수정 #86

--- FE/src/Components/KYH/SignIn/OneClickLogInContainer.tsx
+++ FE/src/Components/KYH/SignIn/OneClickLogInContainer.tsx
@@ -5,7 +5,7 @@ import googleCircleIcon from 'Assets/googleCircleIcon.png';
 export default function OneClickLogInContainer() {
   return (
     <>
-      <div className="flex items-center justify-center w-full mt-4">
+      <div className="flex items-center justify-center w-full mt-8">
         <div className="w-1/3 mt-4 border-b-2 border-gray-500" />
         <span className="mx-2 mt-4 text-gray-500">간편로그인</span>
         <div className="w-1/3 mt-4 border-b-2 border-gray-500" />
