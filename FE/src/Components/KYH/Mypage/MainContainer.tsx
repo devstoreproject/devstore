3ad1acdbe97e275bc95441261d55878243fcd760
kimyonghui <kyhui1115@gmail.com>
2023-08-10 16:05:51 +0900

    [feat] 사용자 프로필 조회 커스텀 훅 구현 및 닉네임 UI 반영 #102

--- FE/src/Components/KYH/Mypage/MainContainer.tsx
+++ FE/src/Components/KYH/Mypage/MainContainer.tsx
@@ -5,9 +5,7 @@ export default function MainContainer() {
   return (
     <div className="flex ml-28">
       <MainNav />
-      <div className="w-3/5">
-        <Outlet />
-      </div>
+      <Outlet />
     </div>
   );
 }
--- FE/src/Components/KYH/Mypage/ProfileContainer.tsx
+++ FE/src/Components/KYH/Mypage/ProfileContainer.tsx
@@ -1,17 +1,19 @@
+import useFetchProfile from 'hooks/mypage/useFetchProfile';
 import { AiFillCamera } from 'react-icons/ai';
-import naverCircleIcon from 'Assets/naverCircleIcon.png';
 
 export default function ProfileContainer() {
+  const profile = useFetchProfile();
+
   return (
     <div className="flex items-center mb-10 ml-28">
       <div className="relative w-20 h-20 mr-4 bg-white border border-black rounded-full">
-        <img src={naverCircleIcon} />
-        <div className="absolute right-0 flex items-center justify-center w-5 h-5 text-xs text-white bg-gray-600 rounded-full bottom-1">
+        <img src={profile.profileImage === null ? '' : profile.profileImage} />
+        <button className="absolute right-0 flex items-center justify-center w-5 h-5 text-xs text-white bg-gray-600 rounded-full bottom-1">
           <AiFillCamera />
-        </div>
+        </button>
       </div>
       <div>
-        <span className="font-bold">여수밤바다</span> 님 어서오세요!
+        <span className="font-bold">{profile.nickname}</span> 님 어서오세요!
       </div>
     </div>
   );
