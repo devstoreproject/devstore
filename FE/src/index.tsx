2cf1a30313e994f1f1d94ca5a854835ffa8910fd
kimyonghui <kyhui1115@gmail.com>
2023-08-10 15:58:12 +0900

    [feat] react-cookie 설정

--- FE/src/index.tsx
+++ FE/src/index.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'index.css';
+import { CookiesProvider } from 'react-cookie';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <CookiesProvider>
+    <App />
+  </CookiesProvider>
+);
