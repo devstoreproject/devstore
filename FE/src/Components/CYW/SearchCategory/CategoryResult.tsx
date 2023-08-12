37b9113ccf57c2ca3197ca79a94fae5507c60791
cyweyo <cyw7715@gmail.com>
2023-08-12 18:38:33 +0900

    [feat] 서버 응답 데이버 페이지 렌더링 구현 및 데이터 타입 지정

--- FE/src/Components/CYW/SearchCategory/CategoryResult.tsx
+++ FE/src/Components/CYW/SearchCategory/CategoryResult.tsx
@@ -1,16 +1,16 @@
-import ResultItem from './ResultItem';
+import type { CategoryContent } from 'Pages/CYW/SearchCategory';
+import CategoryItem from './CategoryItem';
 
-export default function CategoryResult() {
+interface OwnProps {
+  category: CategoryContent[] | null;
+}
+
+export default function CategoryResult({ category }: OwnProps) {
   return (
-    <div className="flex flex-wrap justify-between pt-16 pb-16 w-365">
-      <ResultItem />
-      <ResultItem />
-      <ResultItem />
-      <ResultItem />
-      <ResultItem />
-      <ResultItem />
-      <ResultItem />
-      <ResultItem />
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 pt-16">
+      {category?.map((categoryItem) => (
+        <CategoryItem key={categoryItem.itemId} categoryItem={categoryItem} />
+      ))}
     </div>
   );
 }
