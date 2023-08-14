372a6796c6af5a9f05c82806e61af207dcf8ca01
SigLee <lgh941107@gmail.com>
2023-08-14 23:42:32 +0900

    [test]: Cart 통합 테스트 post

--- BE/backend/src/main/java/project/main/webstore/domain/cart/dto/CartItemDto.java
+++ BE/backend/src/main/java/project/main/webstore/domain/cart/dto/CartItemDto.java
@@ -1,10 +1,12 @@
 package project.main.webstore.domain.cart.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class CartItemDto {
     private Long optionId;
     private int itemCount;
