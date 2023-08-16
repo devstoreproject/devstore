92d79a0e90ef08ae45a89c1056500b0888a6518e
SigLee <lgh941107@gmail.com>
2023-08-16 16:08:38 +0900

    [fit]: 생성자 추가 및 userId 제거

--- BE/backend/src/main/java/project/main/webstore/domain/cart/dto/CartPatchRequestDto.java
+++ BE/backend/src/main/java/project/main/webstore/domain/cart/dto/CartPatchRequestDto.java
@@ -1,11 +1,17 @@
 package project.main.webstore.domain.cart.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 public class CartPatchRequestDto {
     private List<CartItemDto> itemList = new ArrayList<>();
+
+    public CartPatchRequestDto(List<CartItemDto> itemList) {
+        this.itemList = itemList;
+    }
 }
