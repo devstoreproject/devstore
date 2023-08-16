c3ae1a8d7eb5b17ba7b5cc0427bcb0be06e2b0e2
mya413 <lsm940413@gmail.com>
2023-08-16 17:59:32 +0900

    [feat] 공지사항 form 이미지 post 요청 구현, patch 요청 구현 진행중

--- FE/src/Components/LSM/Notice/NoticeForm/NoticeForm.tsx
+++ FE/src/Components/LSM/Notice/NoticeForm/NoticeForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import Button from './Button';
 import ImageInput from './ImageInput';
 import TitleInput from './TitleInput';
@@ -7,14 +7,25 @@ import Select from './Select';
 import Textarea from './Textarea';
 import api from 'api';
 
-export default function NoticeForm() {
+interface NoticeEditProp {
+  datas: any;
+}
+
+export default function NoticeForm({ datas }: NoticeEditProp) {
   const navigate = useNavigate();
+
+  const { id } = useParams() as { id: string };
 
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [category, setCategory] = useState('');
+  const [image, setImage] = useState<string | Blob>('');
 
-  const submitData = async (e: React.FormEvent<HTMLFormElement>) => {
+  const [editTitle, setEditTitle] = useState('');
+  const [editContent, setEditContent] = useState('');
+  const [editCategory, setEditCategory] = useState('');
+
+  const postData = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (category === '') {
@@ -23,6 +34,8 @@ export default function NoticeForm() {
     }
 
     const formData = new FormData();
+
+    formData.append('image', image);
 
     const contents = {
       userId: '2',
@@ -51,15 +64,43 @@ export default function NoticeForm() {
     }
   };
 
+  const patchData = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (category === '') {
+      alert('카테고리를 선택해주세요!🤔');
+      return;
+    }
+
+    const contents = {
+      userId: '2',
+      editTitle,
+      editContent,
+      editCategory,
+    };
+
+    try {
+      await api.patch(`/api/notices/${id}`, contents).then((res) => {
+        console.log(res);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <form className="flex flex-col">
       <div className="flex items-center mb-4">
-        <Select setCategory={setCategory} />
-        <TitleInput setTitle={setTitle} />
+        <Select
+          category={category}
+          setCategory={setCategory}
+          setEditCategory={setEditCategory}
+        />
+        <TitleInput setTitle={setTitle} setEditTitle={setEditTitle} />
       </div>
-      <Textarea setContent={setContent} />
-      <ImageInput />
-      <Button submitData={submitData} />
+      <Textarea setContent={setContent} setEditContent={setEditContent} />
+      <ImageInput setImage={setImage} />
+      <Button postData={postData} patchData={patchData} />
     </form>
   );
 }
