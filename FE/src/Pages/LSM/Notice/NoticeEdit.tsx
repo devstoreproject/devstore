050a6247a6ce8cfe26c804d482ad3593cd671865
mya413 <lsm940413@gmail.com>
2023-08-16 17:53:27 +0900

    [feat] 공지사항 수정 페이지 데이터 get 요청 #96

--- FE/src/Pages/LSM/Notice/NoticeEdit.tsx
+++ FE/src/Pages/LSM/Notice/NoticeEdit.tsx
@@ -1,11 +1,37 @@
+import { useState, useEffect } from 'react';
+import { useParams, useLocation } from 'react-router-dom';
 import NoticeForm from 'Components/LSM/Notice/NoticeForm/NoticeForm';
 import NoticeTitle from 'Components/LSM/Notice/NoticeTitle';
+import api from 'api';
 
 export default function NoticeEdit() {
+  const [datas, setDatas] = useState<any[]>([]);
+
+  const { id } = useParams() as { id: string };
+
+  const path = useLocation();
+  const pathName = path.pathname.slice(14);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await api.get(`/api/notices/${id}`);
+        console.log(res);
+        setDatas(res.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (id !== undefined) {
+      void fetchData();
+    }
+  }, [id]);
+
   return (
     <div className="w-250">
       <NoticeTitle />
-      <NoticeForm />
+      {pathName === 'edit' && <NoticeForm datas={datas} />}
     </div>
   );
 }
