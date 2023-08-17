ebc600bbce3db8c349159cf415d1b2d2a52313e0
kimyonghui <kyhui1115@gmail.com>
2023-08-17 12:34:25 +0900

    [refactor] onChange 방식 변경

--- FE/src/Components/KYH/SignUp/EmailContainer.tsx
+++ FE/src/Components/KYH/SignUp/EmailContainer.tsx
@@ -5,19 +5,17 @@ export default function EmailContainer({
   setEmail,
   isEmailValid,
 }: Email) {
-  const EmailInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  };
-
   return (
     <div className="flex mt-2">
-      <div className="flex flex-col w-4/6">
+      <div className="flex flex-col w-full mr-2">
         <span className="ml-2">이메일</span>
         <input
           type="text"
           className="h-10 pl-2 border border-black"
           placeholder="이메일을 입력하세요"
-          onChange={EmailInputHandler}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
           value={email}
         ></input>
         {isEmailValid ? null : (
@@ -26,9 +24,6 @@ export default function EmailContainer({
           </span>
         )}
       </div>
-      <button className="h-10 px-3 mt-6 ml-3 text-sm bg-gray-300 rounded-xl shadow-btn active:shadow-none active:ml-3.5 active:mt-6.5 duration-100">
-        인증코드전송
-      </button>
     </div>
   );
 }
