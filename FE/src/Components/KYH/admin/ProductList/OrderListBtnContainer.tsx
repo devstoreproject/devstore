402eec5c4cb05d86d680ddfc8522193e8df2832d
kimyonghui <kyhui1115@gmail.com>
2023-08-23 13:11:24 +0900

    [feat] 판매상품 삭제기능(다중 선택) 구현 #110

--- FE/src/Components/KYH/admin/ProductList/OrderListBtnContainer.tsx
+++ FE/src/Components/KYH/admin/ProductList/OrderListBtnContainer.tsx
@@ -1,13 +1,38 @@
+import api from 'api';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-export default function OrderListBtnContainer() {
+interface OwnProps {
+  checkedId: number[];
+}
+
+export default function OrderListBtnContainer({ checkedId }: OwnProps) {
   const navigate = useNavigate();
-  const deleteBtnHandler = () => {};
-  const postBtnHandler = () => {
-    navigate('/admin/productlist/post');
-  };
-  const editBtnHandler = () => {
-    navigate('/admin/productlist/edit');
+  const Authorization = localStorage.getItem('authorization');
+
+  const deleteBtnHandler = () => {
+    const userConfirmed = window.confirm('삭제하시겠습니까?');
+
+    if (userConfirmed) {
+      const fetchDelete = checkedId.slice(1).map(async (id) => {
+        await api.delete(`/api/items/${id}`, {
+          headers: {
+            Authorization,
+          },
+        });
+      });
+
+      axios
+        .all(fetchDelete)
+        .then(() => {
+          window.alert('삭제되었습니다.');
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      window.alert('취소되었습니다.');
+    }
   };
 
   return (
@@ -20,13 +45,17 @@ export default function OrderListBtnContainer() {
       </button>
       <button
         className="w-40 h-10 mr-6 text-white bg-gray-700 rounded-3xl"
-        onClick={editBtnHandler}
+        onClick={() => {
+          navigate('/admin/productlist/edit');
+        }}
       >
         선택 상품 수정하기
       </button>
       <button
         className="w-40 h-10 text-white bg-gray-700 rounded-3xl"
-        onClick={postBtnHandler}
+        onClick={() => {
+          navigate('/admin/productlist/post');
+        }}
       >
         상품 등록하기
       </button>
--- FE/src/Components/KYH/admin/ProductList/TableContents.tsx
+++ FE/src/Components/KYH/admin/ProductList/TableContents.tsx
@@ -1,17 +1,44 @@
 import type { Product } from 'model/auth';
 
+interface OwnProps extends Product {
+  setIsDetailModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setProductId: React.Dispatch<React.SetStateAction<number>>;
+  setCheckedId: React.Dispatch<React.SetStateAction<number[]>>;
+}
+
 export default function TableContents({
   itemId,
   name,
   defaultCount,
   totalCount,
   itemPrice,
-}: Product) {
+  setIsDetailModalOpen,
+  setProductId,
+  setCheckedId,
+}: OwnProps) {
   return (
     <li className="flex items-center justify-between h-12 px-6 border-b border-b-gray-400">
-      <input type="checkbox" className="w-5 h-5" />
+      <input
+        type="checkbox"
+        className="w-5 h-5"
+        onChange={(e) => {
+          if (e.target.checked) {
+            setCheckedId((prev) => [...prev, itemId]);
+          } else {
+            setCheckedId((prev) => prev.filter((id) => id !== itemId));
+          }
+        }}
+      />
       <span>{itemId}</span>
-      <p className="text-center text-gray-700 truncate w-120">{name}</p>
+      <input
+        className="text-center text-gray-700 underline truncate cursor-pointer w-120 decoration-2 underline-offset-4"
+        value={name}
+        type="button"
+        onClick={() => {
+          setIsDetailModalOpen(true);
+          setProductId(itemId);
+        }}
+      />
       <span className="w-16 text-center">{totalCount}</span>
       <span className="w-12 text-center">데이터 필요</span>
       <span className="w-12 text-center">{totalCount}</span>
