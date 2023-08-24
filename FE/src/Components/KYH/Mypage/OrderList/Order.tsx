3e94d6d08d6eed6497248342b19b7e0d75ffcaf8
kimyonghui <kyhui1115@gmail.com>
2023-08-24 14:36:51 +0900

    [design] 주문목록, 리뷰 통합 및 UI 수정

--- FE/src/Components/KYH/Mypage/OrderList/Order.tsx
+++ FE/src/Components/KYH/Mypage/OrderList/Order.tsx
@@ -1,14 +1,14 @@
-export default function Review() {
+export default function Order() {
   return (
     <li className="flex pb-4 mb-4 border-b border-b-gray-300">
       <img className="w-32 mr-10 bg-gray-300 h-28 rounded-xl" />
-      <div className="flex flex-col w-104">
+      <div className="flex flex-col w-128">
         <p className="mt-2 text-sm text-gray-500">
           알파스캔 AOC Q32V3S QHD IPS 75 시력보호 무결점 외 3개
         </p>
         <span className="text-sm text-gray-500">116,000원 | 4개</span>
         {/* <p className="mt-4 text-sm">사용해보니 좋습니다!</p> */}
-        <button className="h-10 pl-4 mt-4 text-sm text-left text-gray-500 bg-white border border-gray-300 rounded-3xl">
+        <button className="h-10 pl-4 mt-4 text-sm text-left text-gray-500 bg-white border border-gray-300 rounded-3xl w-120">
           리뷰 작성하러 가기 {'>'}
         </button>
       </div>
