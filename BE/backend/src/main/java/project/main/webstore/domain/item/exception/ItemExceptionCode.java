bf6ff4d873740f3d520d7cfdcfbf4fe537fa3942
SigLee <lgh941107@gmail.com>
2023-08-26 06:37:50 +0900

    [feat]: 상품 예외 enum 추가

--- BE/backend/src/main/java/project/main/webstore/domain/item/exception/ItemExceptionCode.java
+++ BE/backend/src/main/java/project/main/webstore/domain/item/exception/ItemExceptionCode.java
@@ -8,9 +8,9 @@ import project.main.webstore.exception.ExceptionCode;
 @Getter
 @AllArgsConstructor
 public enum ItemExceptionCode implements ExceptionCode {
-    ITEM_NOT_ENOUGH(HttpStatus.BAD_REQUEST,"상품 수량 부족")
+    ITEM_NOT_ENOUGH(HttpStatus.BAD_REQUEST,"상품 수량 부족"),
 
-    ;
+    ITEM_NOT_SAME(HttpStatus.NOT_FOUND, "상품을 찾을 수 없습니다.");
     private final HttpStatus httpStatus;
     private final String message;
 }
