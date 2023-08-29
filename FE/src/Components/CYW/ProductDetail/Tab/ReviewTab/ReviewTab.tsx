fdabfcad8ade4848538b34b0a271240357066384
cyweyo <cyw7715@gmail.com>
2023-08-29 20:36:55 +0900

    [feat] 리뷰 등록, 삭제 기능 추가

--- FE/src/Components/CYW/ProductDetail/Tab/ReviewTab/ReviewTab.tsx
+++ FE/src/Components/CYW/ProductDetail/Tab/ReviewTab/ReviewTab.tsx
@@ -6,15 +6,16 @@ import type { ReviewContentType } from '../Tab';
 interface OwnProps {
   tab: number;
   review: ReviewContentType[] | null;
+  setReview: React.Dispatch<React.SetStateAction<ReviewContentType[] | null>>;
 }
 
-export default function ReviewTab({ tab, review }: OwnProps) {
+export default function ReviewTab({ tab, review, setReview }: OwnProps) {
   return tab === 1 ? (
     <div className="bg-slate-100 rounded-lg">
       <p className="border-b-2 py-4 pl-8">상품 리뷰</p>
-      <ReviewElement review={review} />
+      <ReviewElement review={review} setReview={setReview} />
       <PaginationContainer />
-      <ReviewRegister />
+      <ReviewRegister review={review} setReview={setReview} />
     </div>
   ) : null;
 }
