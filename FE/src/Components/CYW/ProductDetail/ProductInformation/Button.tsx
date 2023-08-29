c0d36acf0151be0f41f6ed3a4c90523cea787fcb
cyweyo <cyw7715@gmail.com>
2023-08-29 20:24:45 +0900

    [feat] 좋아요 기능 추가

--- FE/src/Components/CYW/ProductDetail/ProductInformation/Button.tsx
+++ FE/src/Components/CYW/ProductDetail/ProductInformation/Button.tsx
@@ -1,17 +1,44 @@
+import api from 'api';
 import { useState } from 'react';
 import { VscHeart, VscHeartFilled } from 'react-icons/vsc';
+import { useParams } from 'react-router-dom';
 
 export default function Button() {
   const [heart, setHeart] = useState(false);
+  const userId: string | null = localStorage.getItem('userId');
   const heartSize = 27;
+  const { id } = useParams();
+
+  const likeHandler = () => {
+    if (userId === null) {
+      alert('로그인 후에 좋아해 주세요.');
+      return;
+    }
+
+    if (userId !== null && id !== undefined) {
+      api
+        .post(
+          `api/items/${id}/favorite?userId=${userId}`,
+          {},
+          {
+            headers: {
+              Authorization: localStorage.getItem('authorization'),
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res);
+          setHeart(!heart);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
 
   return (
     <div className="flex pt-5 items-center justify-end">
-      <button
-        onClick={() => {
-          setHeart(!heart);
-        }}
-      >
+      <button onClick={likeHandler}>
         {heart ? (
           <VscHeartFilled size={heartSize} />
         ) : (
