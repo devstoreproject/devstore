43b95b0e9de977164060bf1091dfd8d50082e8d6
cyweyo <cyw7715@gmail.com>
2023-08-29 20:32:38 +0900

    [feat] 문의 내역 수정, 삭제 기능 추가

---
+++ FE/src/Components/CYW/ProductDetail/Tab/InquiryTab/InquiryEditDeleteBtn.tsx
@@ -0,0 +1,98 @@
+import api from 'api';
+import type { InquiryContentType } from '../Tab';
+import { useParams } from 'react-router-dom';
+
+interface OwnProps {
+  i: number;
+  inquiryItem: InquiryContentType;
+  inquiry: InquiryContentType[] | null;
+  setInquiry: React.Dispatch<React.SetStateAction<InquiryContentType[] | null>>;
+  setEdit: React.Dispatch<React.SetStateAction<number | null>>;
+  edit: number | null;
+}
+
+export default function InquiryEditDeleteBtn({
+  i,
+  inquiryItem,
+  inquiry,
+  setInquiry,
+  setEdit,
+  edit,
+}: OwnProps) {
+  const { id } = useParams();
+
+  const deleteHandler = () => {
+    const confirmDelete = window.confirm('정말로 삭제하시겠습니까?');
+    const deleteApiUrl = `api/qna/${inquiryItem.questionId}`;
+
+    if (confirmDelete) {
+      api
+        .delete(deleteApiUrl, {
+          headers: {
+            Authorization: localStorage.getItem('authorization'),
+          },
+        })
+        .then((res) => {
+          setInquiry((prevInquiry) => {
+            if (prevInquiry === null) {
+              return [];
+            }
+            return prevInquiry.filter((item, index) => index !== i);
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
+  const editHandler = (i: number) => {
+    if (edit === i && inquiry !== null) {
+      const editedInquiry = [...inquiry];
+      const comment = editedInquiry[i].comment;
+      api
+        .patch(
+          `api/qna/items/${id as string}/${inquiryItem.questionId}`,
+          {
+            comment,
+          },
+          {
+            headers: {
+              Authorization: localStorage.getItem('authorization'),
+            },
+          }
+        )
+        .then(() => {
+          setEdit(null);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      setEdit(i);
+    }
+  };
+
+  return (
+    <>
+      <div className="absolute right-14">
+        <button
+          className="border border-black rounded-lg px-2"
+          onClick={() => {
+            editHandler(i);
+          }}
+        >
+          수정
+        </button>
+      </div>
+      <div className="absolute right-2">
+        <button
+          className="border border-black rounded-lg px-2"
+          onClick={deleteHandler}
+        >
+          삭제
+        </button>
+      </div>
+    </>
+  );
+}
