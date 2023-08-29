874538ffda399ce6fa07ebf0c26e7098a9518944
mya413 <lsm940413@gmail.com>
2023-08-29 23:28:51 +0900

    [chore] tailwind.config 일부 수정

--- FE/tailwind.config.js
+++ FE/tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
+    './Components/**/*.{js,ts,jsx,tsx}',
+    './Pages/**/*.{js,ts,jsx,tsx}',
+  ],
 
   theme: {
     screens: {
