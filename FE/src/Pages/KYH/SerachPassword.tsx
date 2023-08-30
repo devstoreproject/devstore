aae82c3733fdf1ba772aa5493b32d44434a8b421
kimyonghui <kyhui1115@gmail.com>
2023-08-30 15:57:32 +0900

    [design] 비밀번호 찾기 디자인 및 기능 구현 중 #86

---
+++ FE/src/Pages/KYH/SerachPassword.tsx
@@ -0,0 +1,9 @@
+import SearchPasswordForm from 'Components/KYH/SignIn/SearchPasswordForm';
+
+export default function SearchPassword() {
+  return (
+    <div className="flex justify-center h-screen bg-light-gray">
+      <SearchPasswordForm />
+    </div>
+  );
+}
