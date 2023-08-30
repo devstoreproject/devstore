059e859264365f6aa98c6a0809b267b892d1ca8b
kimyonghui <kyhui1115@gmail.com>
2023-08-30 15:43:07 +0900

    [feat] 마이페이지 주문목록 관련 데이터 변환 기능 분리 #102

--- FE/src/Components/KYH/Mypage/OrderList/OrderItems.tsx
+++ FE/src/Components/KYH/Mypage/OrderList/OrderItems.tsx
@@ -1,13 +1,11 @@
 import OrderItem from './OrderItem';
-// import useFetchReviews from 'hooks/admin/product/useFetchReviews';
 import useFetchOrders from 'hooks/mypage/useFetchOrders';
 
 export default function OrderItems() {
   const orders = useFetchOrders();
-  // const reviews = useFetchReviews();
 
   return (
-    <div className="flex flex-col mt-14">
+    <div className="flex flex-col">
       <span className="mb-4 font-bold">주문 목록</span>
       <ul className="w-200">
         {orders.map((order) => (
@@ -17,6 +15,7 @@ export default function OrderItems() {
             discountedPrice={order.discountedPrice}
             createdAt={order.createdAt}
             orderItemList={order.orderItemList}
+            ordersStatus={order.ordersStatus}
           />
         ))}
       </ul>
