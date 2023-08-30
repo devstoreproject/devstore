882cd9caa7dc854e7d8357f998f7322d5e315007
kimyonghui <kyhui1115@gmail.com>
2023-08-30 15:35:40 +0900

    [feat] 관리자 메인페이지 문의내용 조회 기능 구현 #104

--- FE/src/Components/KYH/admin/AdminMain/InquiryList.tsx
+++ FE/src/Components/KYH/admin/AdminMain/InquiryList.tsx
@@ -1,12 +1,20 @@
 import InquiryContent from './InquiryContent';
+import useFetchInquiry from 'hooks/admin/inquiry/useFetchInquiry';
 
 export default function InquiryList() {
+  const inquiry = useFetchInquiry();
+  console.log(inquiry);
+
   return (
     <div className="flex flex-col h-48 px-6 py-4 mt-auto bg-white rounded-lg shadow-signBox w-200">
-      <span className="mb-4 text-lg font-bold">문의내역</span>
-      <InquiryContent />
-      <InquiryContent />
-      <InquiryContent />
+      <span className="mb-2 text-lg font-bold">문의내역</span>
+      {inquiry.slice(0, 4).map((content) => (
+        <InquiryContent
+          key={content.questionId}
+          comment={content.comment}
+          answer={content.answer}
+        />
+      ))}
     </div>
   );
 }
