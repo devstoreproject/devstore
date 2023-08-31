19bcfb02d44635318713c906e3424ca435be1f25
kimyonghui <kyhui1115@gmail.com>
2023-08-31 22:24:45 +0900

    [feat] 관리자 메인페이지 판매현황 조회 기능 구현 #104

--- FE/src/Components/KYH/admin/AdminMain/OrderStatus.tsx
+++ FE/src/Components/KYH/admin/AdminMain/OrderStatus.tsx
@@ -1,26 +1,18 @@
-export default function OrderStatus() {
+import orderStatusCount from 'utils/admin/order/orderStatusCount';
+
+interface OwnProps {
+  status: string;
+  statusName: string;
+  style: string;
+}
+
+export default function OrderStatus({ status, statusName, style }: OwnProps) {
   return (
-    <div className="flex h-24 w-200">
-      <div className="flex flex-col items-center w-1/5 h-full mr-2 bg-white rounded-lg shadow-signBox">
-        <span className="mt-3 text-gray-500">입금대기</span>
-        <span className="mt-2 text-3xl">102</span>
-      </div>
-      <div className="flex flex-col items-center w-1/5 h-full mr-2 bg-white rounded-lg shadow-signBox">
-        <span className="mt-3 text-gray-500">결제완료</span>
-        <span className="mt-2 text-3xl">99</span>
-      </div>
-      <div className="flex flex-col items-center w-1/5 h-full mr-2 bg-white rounded-lg shadow-signBox">
-        <span className="mt-3 text-gray-500">배송중</span>
-        <span className="mt-2 text-3xl">1102</span>
-      </div>
-      <div className="flex flex-col items-center w-1/5 h-full mr-2 bg-white rounded-lg shadow-signBox">
-        <span className="mt-3 text-gray-500">교환요청</span>
-        <span className="mt-2 text-3xl">12</span>
-      </div>
-      <div className="flex flex-col items-center w-1/5 h-full bg-white rounded-lg shadow-signBox">
-        <span className="mt-3 text-gray-500">환불요청</span>
-        <span className="mt-2 text-3xl">8</span>
-      </div>
+    <div
+      className={`flex flex-col items-center w-1/5 h-full bg-white rounded-lg shadow-signBox ${style}`}
+    >
+      <span className="mt-3 text-gray-500">{statusName}</span>
+      <span className="mt-2 text-3xl">{orderStatusCount(status)}</span>
     </div>
   );
 }
