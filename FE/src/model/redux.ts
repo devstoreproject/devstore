14a58516cce0878107bcb543970687b8c19cb12b
kimyonghui <kyhui1115@gmail.com>
2023-09-04 21:48:48 +0900

    [feat] ItemId 관련 redux 설정

---
+++ FE/src/model/redux.ts
@@ -0,0 +1,4 @@
+export interface StoreType {
+  currentAddress: number;
+  currentItemId: number;
+}
--- FE/src/store/store.ts
+++ FE/src/store/store.ts
@@ -1,6 +1,12 @@
-import { legacy_createStore as createStore } from 'redux';
+import { combineReducers, legacy_createStore as createStore } from 'redux';
 import currentAddress from './modules/setCurrentIndex';
+import currentItemId from './modules/setItemId';
 
-const store = createStore(currentAddress);
+const reducers = combineReducers({
+  currentAddress,
+  currentItemId,
+});
+
+const store = createStore(reducers);
 
 export default store;
