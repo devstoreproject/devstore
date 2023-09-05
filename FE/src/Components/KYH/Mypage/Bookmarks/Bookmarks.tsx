bb9f3f2075e8237b1d3d00770cd9cdfb380d6600
kimyonghui <kyhui1115@gmail.com>
2023-09-05 16:26:57 +0900

    [feat] 마이페이지 찜 목록 조회, 찜 추가 삭제 기능 구현 #102

--- FE/src/Components/KYH/Mypage/Bookmarks/Bookmarks.tsx
+++ FE/src/Components/KYH/Mypage/Bookmarks/Bookmarks.tsx
@@ -1,15 +1,22 @@
+import useFetchBookmarks from 'hooks/mypage/useFetchBookmarks';
 import BookmarkItem from './BookmarkItem';
 
 export default function Bookmarks() {
+  const bookmarks = useFetchBookmarks();
   return (
     <div className="flex flex-col">
       <span className="mb-4 font-bold">찜 목록</span>
       <ul className="flex flex-wrap">
-        <BookmarkItem />
-        <BookmarkItem />
-        <BookmarkItem />
-        <BookmarkItem />
-        <BookmarkItem />
+        {bookmarks.map((bookmark) => (
+          <BookmarkItem
+            key={bookmark.itemId}
+            itemId={bookmark.itemId}
+            name={bookmark.name}
+            itemPrice={bookmark.itemPrice}
+            like={bookmark.like}
+            imageList={bookmark.imageList}
+          />
+        ))}
       </ul>
     </div>
   );
