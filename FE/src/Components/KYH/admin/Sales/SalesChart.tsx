94381344d8b8991ca27461ff4d08007f77ff84f3
kimyonghui <kyhui1115@gmail.com>
2023-09-05 18:00:35 +0900

    [feat] 차트 관련 프롭스 설정

--- FE/src/Components/KYH/admin/Sales/SalesChart.tsx
+++ FE/src/Components/KYH/admin/Sales/SalesChart.tsx
@@ -1,11 +1,27 @@
-import Chart from './Chart';
-import SalesChartHeader from './SalesChartHeader';
+import { useState } from 'react';
+import useFetchSales from 'hooks/admin/sales/useFetchSales';
+import SalesChartHeader from '../AdminMain/SalesChartHeader';
+import Chart from '../AdminMain/Chart';
+import useFetchProducts from 'hooks/admin/product/useFetchProducts';
 
 export default function SalesChart() {
+  const [salesStatus, setSalesStatus] = useState([false, true]);
+  const sales = useFetchSales(salesStatus);
+  const products = useFetchProducts();
+  console.log(products);
   return (
     <div className="px-6 py-4 mb-4 bg-white border border-gray-400 rounded-lg w-300">
-      <SalesChartHeader />
-      <Chart />
+      <SalesChartHeader
+        salesStatus={salesStatus}
+        setSalesStatus={setSalesStatus}
+      />
+      <Chart
+        sales={sales}
+        salesStatus={salesStatus}
+        width={1100}
+        height={350}
+        margin={{ left: 20, top: 30, right: 20 }}
+      />
     </div>
   );
 }
