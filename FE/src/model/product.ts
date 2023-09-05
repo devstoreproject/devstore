eb7f49fe8e04bcaf2f18c97155376933313ed51e
kimyonghui <kyhui1115@gmail.com>
2023-09-05 16:33:58 +0900

    [feat] 타입 분리

--- FE/src/model/product.ts
+++ FE/src/model/product.ts
@@ -4,3 +4,16 @@ export interface Product {
   itemPrice: number;
   totalCount: number;
 }
+
+export interface Bookmark {
+  itemId: number;
+  name: string;
+  like: boolean;
+  itemPrice: number;
+  imageList: image[];
+}
+
+interface image {
+  imageId: number;
+  thumbnailPath: string;
+}
---
+++ FE/src/model/sales.ts
@@ -0,0 +1,4 @@
+export interface Sales {
+  localDate: string;
+  totalOriginalPrice: number;
+}
