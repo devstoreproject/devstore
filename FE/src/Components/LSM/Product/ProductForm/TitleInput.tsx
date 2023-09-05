9f69a3b03bc5e6fa7f9c705c9ee9664946fcd1a6
mya413 <lsm940413@gmail.com>
2023-09-05 14:26:18 +0900

    [feat] 상품 상품명 인풋 구현 #88

--- FE/src/Components/LSM/Product/ProductForm/TitleInput.tsx
+++ FE/src/Components/LSM/Product/ProductForm/TitleInput.tsx
@@ -1,4 +1,13 @@
-export default function TitleInput() {
+interface ProductProp {
+  // datas: any;
+  // path: string;
+  setTitle: React.Dispatch<React.SetStateAction<string>>;
+  // setEditTitle: React.Dispatch<React.SetStateAction<string>>;
+}
+export default function TitleInput({ setTitle }: ProductProp) {
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
   return (
     <div className="flex items-center">
       <label htmlFor="productTitle" className="w-20 text-label-gray">
@@ -8,7 +17,8 @@ export default function TitleInput() {
         id="productTitle"
         type="text"
         placeholder="상품명을 입력해 주세요"
-        className="px-5 py-3 border lg:w-full rounded-3xl border-gray"
+        onChange={onChangeHandler}
+        className="w-full px-5 py-3 border rounded-3xl border-gray"
       />
     </div>
   );
