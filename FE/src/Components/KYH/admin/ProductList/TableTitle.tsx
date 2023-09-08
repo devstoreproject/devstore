65248c9763f0112c85aa2861228532171fb752a4
kimyonghui <kyhui1115@gmail.com>
2023-09-08 14:00:40 +0900

    [fix] 필요없는 값 제거

--- FE/src/Components/KYH/admin/ProductList/TableTitle.tsx
+++ FE/src/Components/KYH/admin/ProductList/TableTitle.tsx
@@ -2,12 +2,12 @@ export default function TableTitle() {
   return (
     <div className="flex items-center justify-between h-12 px-6 bg-white border-b rounded-t-lg border-b-gray-400">
       <div className="w-5"></div>
-      <div className="text-white">1</div>
+      <div className="w-5"></div>
       <span className="text-center text-gray-600 w-120">상품명</span>
       <span className="w-16 text-center text-gray-600">총 수량</span>
       <span className="w-12 text-center text-gray-600">판매량</span>
       <span className="w-12 text-center text-gray-600">재고</span>
-      <span className="w-24 text-center text-gray-600">금액</span>
+      <span className="text-center text-gray-600 w-28">금액</span>
     </div>
   );
 }
