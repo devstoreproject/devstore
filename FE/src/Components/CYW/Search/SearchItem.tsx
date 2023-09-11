dbd28c8b96c7a080268e0be9b414ba9fc314509f
cyweyo <cyw7715@gmail.com>
2023-09-11 16:01:10 +0900

    [feat] 상품명으로 검색 기능 추가

--- FE/src/Components/CYW/Search/SearchItem.tsx
+++ FE/src/Components/CYW/Search/SearchItem.tsx
@@ -1,17 +1,39 @@
-export default function SearchItem() {
+import type { SearchType } from 'Pages/CYW/Search';
+import { useNavigate } from 'react-router-dom';
+
+interface OwnProps {
+  searchItem: SearchType | null;
+  i: number;
+}
+
+export default function SearchItem({ searchItem, i }: OwnProps) {
+  const navigate = useNavigate();
+  const itemPrice = searchItem?.itemPrice;
+  const itemName = searchItem?.name;
+  const deliveryPrice = searchItem?.deliveryPrice;
+
   return (
     <div className="flex flex-col py-6 px-6 bg-white w-72 rounded-xl shadow-md">
       <img
-        src="https://item.sellpro.co.kr/data/item/125/5565685125_550.jpg"
-        className="w-68 h-40 object-cover border rounded-xl mb-4"
+        src={searchItem?.imageList[0]?.thumbnailPath}
+        className="w-68 h-40 object-cover border rounded-xl mb-4 cursor-pointer"
+        onClick={() => {
+          navigate(`${Number(searchItem?.itemId)}`);
+        }}
       />
-      <p>알파스캔 AOC Q32V3S QHD IPS 75 시력보호 무결점</p>
+      <p>{itemName}</p>
       <div>
-        <span className="text-lg">398,000</span>
-        <span className="pl-2 line-through text-slate-500">443,000</span>
+        <span className="text-lg">{itemPrice?.toLocaleString('ko-KR')}</span>
+        <span className="pl-2 line-through text-slate-500">
+          {itemPrice?.toLocaleString('ko-KR')}
+        </span>
         <span className="pl-2 text-slate-500">10%</span>
       </div>
-      <p className="text-slate-500">무료배송</p>
+      <p className="text-slate-500">
+        {deliveryPrice === 0
+          ? '무료 배송'
+          : `배송비 ${String(deliveryPrice?.toLocaleString('ko-KR'))}원`}
+      </p>
     </div>
   );
 }
--- FE/src/Components/CYW/Search/SearchTool.tsx
+++ FE/src/Components/CYW/Search/SearchTool.tsx
@@ -1,13 +1,35 @@
+import type { SearchType } from 'Pages/CYW/Search';
+import api from 'api';
 import { useState } from 'react';
 
-export default function SearchTool() {
-  const [option, setOption] = useState('상품명');
-  const [search, setSearch] = useState('');
-  const searchSubmitHandler = () => {
-    // option, search
+interface OwnProps {
+  option: string;
+  setOption: React.Dispatch<React.SetStateAction<string>>;
+  search: SearchType[] | null;
+  setSearch: React.Dispatch<React.SetStateAction<SearchType[] | null>>;
+}
+
+export default function SearchTool({
+  option,
+  setOption,
+  search,
+  setSearch,
+}: OwnProps) {
+  const [input, setInput] = useState<string>('');
+
+  const searchSubmitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    api
+      .get(`api/items/search/itemName?itemName=${input}&page=0&size=10`)
+      .then((res) => {
+        setSearch(res.data.data.content);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
-  console.log(option, search);
   return (
     <form
       className="flex justify-center pt-5 w-full"
@@ -20,20 +42,20 @@ export default function SearchTool() {
         value={option}
         className="rounded-l-full px-4 border"
       >
-        <option value={'상품명'}></option>
-        <option value={'스펙'}></option>
+        <option value={'상품명'}>상품명</option>
+        {/* <option value={'스펙'}></option> */}
       </select>
       <input
         id="searchTitle"
         onChange={(e) => {
-          setSearch(e.target.value);
+          setInput(e.target.value);
         }}
-        value={search}
+        value={input}
         type="text"
         placeholder="찾고 싶은 상품을 검색해주세요."
-        className="px-5 py-3 border-t border-r border-b lg:w-full rounded-r-full border-gray w-1/3"
+        className="px-5 py-3 border-t border-r border-b w-1/3 rounded-r-full border-gray"
       />
-      <button className="px-24 py-3 border border-gray bg-slate-800 text-slate-50 rounded-3xl ml-2">
+      <button className="px-6 py-3 border border-gray bg-slate-800 text-slate-50 rounded-3xl ml-2">
         검색하기
       </button>
     </form>
