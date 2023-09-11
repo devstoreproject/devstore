c6390dd948fab4bbd1ca3d507edbe78b44755af5
kimyonghui <kyhui1115@gmail.com>
2023-09-11 17:29:55 +0900

    [feat] 관리자 매출관리 페이지 제품별 매출 조회 기능 구현 #116

--- FE/src/Components/KYH/admin/Sales/Sales.tsx
+++ FE/src/Components/KYH/admin/Sales/Sales.tsx
@@ -1,4 +1,3 @@
-import PaginationContainer from './PaginationContainer';
 import SalesChart from './SalesChart';
 import Table from './Table';
 
@@ -7,11 +6,8 @@ export default function Sales() {
     <div className="flex flex-col">
       <span className="mb-6 text-xl font-bold">총 매출내역</span>
       <SalesChart />
-      <span className="mt-10 mb-6 text-xl font-bold">제품별 매출</span>
+      <span className="mt-10 text-xl font-bold">제품별 매출</span>
       <Table />
-      <div className="flex items-center mt-6 w-300">
-        <PaginationContainer />
-      </div>
     </div>
   );
 }
--- FE/src/Components/KYH/admin/Sales/TableContents.tsx
+++ FE/src/Components/KYH/admin/Sales/TableContents.tsx
@@ -1,14 +1,27 @@
-export default function TableContents() {
+import type { ItemSales } from 'model/sales';
+import addCommasToPrice from 'utils/addCommasToPrice';
+import calculateSalesRate from 'utils/sales/calculateSalesRate';
+
+interface OwnProps extends ItemSales {
+  idx: number;
+  totalSales: number;
+}
+
+export default function TableContents({
+  itemName,
+  itemPrice,
+  idx,
+  totalSales,
+}: OwnProps) {
+  const price = addCommasToPrice(itemPrice);
+  const salesRate = calculateSalesRate(itemPrice, totalSales);
   return (
     <li className="flex items-center justify-between h-12 px-6 border-b border-b-gray-400">
-      <input type="checkbox" className="w-5 h-5" />
-      <span>1</span>
-      <p className="text-gray-700 truncate w-120">
-        알파스캔 AOC Q32V3S QHD IPS 75 시력보호 무결점 SRGB 100% 전문가용 모니터
-      </p>
-      <span className="w-16 text-center">150</span>
-      <span className="w-20 text-center">50</span>
-      <span className="w-16 text-center">100</span>
+      <span className="w-10 text-center">{idx + 1}</span>
+      <p className="mr-4 text-center text-gray-700 truncate w-96">{itemName}</p>
+      <span className="w-16 text-center">5</span>
+      <span className="w-40 text-center">{price}원</span>
+      <span className="w-16 text-center">{salesRate}%</span>
     </li>
   );
 }
