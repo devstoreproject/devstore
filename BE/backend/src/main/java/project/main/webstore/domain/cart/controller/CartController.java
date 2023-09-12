1f9c4ba7773821d2a3fa59683705db958f61fa1f
SigLee <lgh941107@gmail.com>
2023-09-13 02:53:24 +0900

    [refactor]: patch 로직 통합 - 기존에 나눠져 있던 삭제 / 추가 / 변경 로직 통합 - 로그인 구현으로 인한url 변경     -api/cart/users/{userId} -> api/cart

--- BE/backend/src/main/java/project/main/webstore/domain/cart/controller/CartController.java
+++ BE/backend/src/main/java/project/main/webstore/domain/cart/controller/CartController.java
@@ -1,58 +1,65 @@
 package project.main.webstore.domain.cart.controller;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import java.net.URI;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import project.main.webstore.domain.cart.dto.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project.main.webstore.domain.cart.dto.CartGetResponseDto;
+import project.main.webstore.domain.cart.dto.CartIdResponseDto;
+import project.main.webstore.domain.cart.dto.CartPatchRequestDto;
+import project.main.webstore.domain.cart.dto.CartPostRequestDto;
+import project.main.webstore.domain.cart.dto.LocalCartDto;
 import project.main.webstore.domain.cart.entity.Cart;
 import project.main.webstore.domain.cart.mapper.CartMapper;
 import project.main.webstore.domain.cart.service.CartService;
 import project.main.webstore.dto.ResponseDto;
 import project.main.webstore.enums.ResponseCode;
+import project.main.webstore.utils.CheckLoginUser;
 import project.main.webstore.utils.UriCreator;
-
-import java.net.URI;
-import java.util.List;
 
 @RequestMapping("/api/cart")
 @RestController
 @RequiredArgsConstructor
 public class CartController {
+    private static final String DEFAULT_URL = "/cart";
     private final CartService cartService;
     private final CartMapper mapper;
 
-    @PostMapping("/users/{userId}")
-    public ResponseEntity<ResponseDto<CartIdResponseDto>> postCart(@RequestBody CartPostRequestDto post, @PathVariable Long userId) {
+    @PostMapping
+    public ResponseEntity<ResponseDto<CartIdResponseDto>> postCart(@RequestBody CartPostRequestDto post,
+                                                                   @Parameter(hidden = true)@AuthenticationPrincipal Object principal) {
+        Long userId = CheckLoginUser.getContextIdx(principal);
         List<LocalCartDto> request = mapper.toLocalList(post.getItemList());
         Cart result = cartService.postCart(request, userId);
 
         CartIdResponseDto response = mapper.toResponseId(result);
-        URI location = UriCreator.createUri("/cart/userId", response.getUserId());
+        URI location = UriCreator.createUri(DEFAULT_URL + "/userId", response.getUserId());
         ResponseDto<CartIdResponseDto> responseDto = ResponseDto.<CartIdResponseDto>builder().data(response).customCode(ResponseCode.CREATED).build();
         return ResponseEntity.created(location).body(responseDto);
     }
 
-    //수량 변경
-    @PatchMapping("/users/{userId}")
-    public ResponseEntity<ResponseDto<CartIdResponseDto>> patchCart(@PathVariable Long userId, @RequestBody CartPatchRequestDto patch) {
-        List<LocalCartDto> request = mapper.toLocalList(patch.getItemList());
-        Cart result = cartService.patchCart(request, userId);
+    @PatchMapping
+    public ResponseEntity<ResponseDto<CartIdResponseDto>> patchCart(@Parameter(hidden = true) @AuthenticationPrincipal Object principal,
+                                                                    @RequestBody CartPatchRequestDto patch) {
+        Long userId = CheckLoginUser.getContextIdx(principal);
+        List<LocalCartDto> request = mapper.toLocalList(patch.getPatchItemList());
+        List<Long> deleteOptionRequestId = mapper.toList(patch.getDeleteOptionId());
+        Cart result = cartService.patchCart(userId,request,deleteOptionRequestId);
         CartIdResponseDto response = mapper.toResponseId(result);
-        URI location = UriCreator.createUri("/cart/userId", response.getUserId());
+        URI location = UriCreator.createUri(DEFAULT_URL + "/userId", response.getUserId());
         ResponseDto<CartIdResponseDto> responseDto = ResponseDto.<CartIdResponseDto>builder().data(response).customCode(ResponseCode.CREATED).build();
         return ResponseEntity.ok().location(location).body(responseDto);
     }
 
-    //일부 삭제
-    @PatchMapping("/users/{userId}/del")
-    public ResponseEntity<ResponseDto<CartIdResponseDto>> patchCart(@PathVariable Long userId, @RequestBody CartDeleteDto delete) {
-
-        Cart result = cartService.deleteCartItem(userId, delete.getDeleteIdList());
-        CartIdResponseDto response = mapper.toResponseId(result);
-        URI location = UriCreator.createUri("/cart/userId", response.getUserId());
-        ResponseDto<CartIdResponseDto> responseDto = ResponseDto.<CartIdResponseDto>builder().data(response).customCode(ResponseCode.CREATED).build();
-        return ResponseEntity.ok().location(location).body(responseDto);
-    }
 
     //조회
     @GetMapping("/users/{userId}")
--- BE/backend/src/main/java/project/main/webstore/domain/cart/mapper/CartMapper.java
+++ BE/backend/src/main/java/project/main/webstore/domain/cart/mapper/CartMapper.java
@@ -1,14 +1,14 @@
 package project.main.webstore.domain.cart.mapper;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 import project.main.webstore.domain.cart.dto.CartGetResponseDto;
 import project.main.webstore.domain.cart.dto.CartIdResponseDto;
 import project.main.webstore.domain.cart.dto.CartItemDto;
 import project.main.webstore.domain.cart.dto.LocalCartDto;
 import project.main.webstore.domain.cart.entity.Cart;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class CartMapper {
@@ -29,5 +29,9 @@ public class CartMapper {
         if (result != null)
             return new CartGetResponseDto(result);
         return null;
+    }
+
+    public List<Long> toList(List<Long> idList) {
+        return idList == null ? new ArrayList<>() : idList;
     }
 }
