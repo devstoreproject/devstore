9b43a2e48152833019fd78cabb6ccf28bf6e371d
mya413 <lsm940413@gmail.com>
2023-09-13 13:50:42 +0900

    [fix] 메인페이지 이미지API 삭제 후 고정이미지 설정 #84

--- FE/src/Components/LSM/Main/ThemeProduct/ThemeItem.tsx
+++ FE/src/Components/LSM/Main/ThemeProduct/ThemeItem.tsx
@@ -18,7 +18,7 @@ export default function ThemeItem({
     backgroundImage: `url('${image}')`,
   };
   const noneBackgroundImage = {
-    backgroundImage: `url('https://source.unsplash.com/random')`,
+    backgroundImage: `url('https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3440&q=80')`,
   };
 
   const navigate = useNavigate();
