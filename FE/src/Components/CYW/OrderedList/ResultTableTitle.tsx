d43ee3960eb60b2eed61410026510620c9acb502
kimyonghui <kyhui1115@gmail.com>
2023-09-13 13:57:15 +0900

    [design] 주문된 상품내역 표 타이틀 디자인 수정

--- FE/src/Components/CYW/OrderedList/ResultTableTitle.tsx
+++ FE/src/Components/CYW/OrderedList/ResultTableTitle.tsx
@@ -4,7 +4,7 @@ export default function ResultTableTitle({
   commonStyle: string;
 }) {
   return (
-    <div className="flex w-full h-12 bg-white border-b rounded-t-lg border-b-gray-400">
+    <div className="flex w-full h-12 font-bold bg-white border-b rounded-t-lg border-b-gray-400">
       <span className={`${commonStyle} w-16`}></span>
       <span className={`${commonStyle} w-72`}>주문 번호</span>
       <span className={`${commonStyle} w-40`}>처리 상태</span>
