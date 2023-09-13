2d9ef47189274d8e99b886d0cf7e7bbddf2b4916
mya413 <lsm940413@gmail.com>
2023-09-13 13:56:46 +0900

    [feat] 상품 수정 폼 기능 구현 #90

--- FE/src/Components/LSM/Product/ProductForm/ProductForm.tsx
+++ FE/src/Components/LSM/Product/ProductForm/ProductForm.tsx
@@ -9,21 +9,42 @@ import TitleInput from './TitleInput';
 import PriceInput from './PriceInput';
 import CategoryInput from './CategoryInput';
 import api from 'api';
+import { useSelector } from 'react-redux';
+import type { StoreType } from 'model/redux';
 
-export default function ProductForm() {
+interface ProductEditProp {
+  datas: any;
+  pathName: string;
+}
+
+export default function ProductForm({ datas, pathName }: ProductEditProp) {
   const navigate = useNavigate();
+  const getItemId = useSelector((e: StoreType) => e.currentItemId);
 
   const [title, setTitle] = useState('');
   const [category, setCategory] = useState('');
-  const [price, setPrice] = useState<any>('');
-  const [imagePreview, setImagePreview] = useState<any>([]);
+  const [price, setPrice] = useState(0);
   const [images, setImages] = useState([]);
-  const [deliveryPrice, setDeliveryPrice] = useState<any>(0);
+  const [imagePreview, setImagePreview] = useState<any>([]);
+  const [deliveryPrice, setDeliveryPrice] = useState(0);
   const [options, setOptions] = useState([
     { optionName: '', itemCount: 0, optionDetail: '', additionalPrice: 0 },
   ]);
   const [defaultCount, setDefaultCount] = useState(0);
   const [contents, setContents] = useState('');
+
+  const [editTitle, setEditTitle] = useState(datas?.name);
+  const [editCategory, setEditCategory] = useState(datas?.category);
+  const [editPrice, setEditPrice] = useState(datas?.itemPrice);
+  const [editImages, setEditImages] = useState(datas?.infoList);
+  const [editDeliveryPrice, setEditDeliveryPrice] = useState(
+    datas?.deliveryPrice
+  );
+  const [editOptions, setEditOptions] = useState(datas?.optionList);
+  const [deleteOptions, setDeleteOptions] = useState<any>([]);
+  const [editContents, setEditContents] = useState(datas?.description);
+
+  console.log(datas);
 
   const postData = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -58,7 +79,6 @@ export default function ProductForm() {
         .post('/api/items', formData, {
           headers: {
             'Content-Type': 'multipart/form-data',
-            Authorization: localStorage.getItem('authorization'),
           },
         })
         .then((res) => {
@@ -68,15 +88,69 @@ export default function ProductForm() {
     } catch (error) {
       console.log(error);
     }
-    console.log(imagePreview, postDatas);
+    console.log(postDatas);
+  };
+
+  const patchData = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const formData = new FormData();
+
+      formData.append('imageList', imagePreview);
+
+      const patchDatas = {
+        name: editTitle,
+        category: editCategory,
+        itemPrice: editPrice,
+        deliveryPrice: editDeliveryPrice,
+        description: editContents,
+        defaultCount,
+        discountRate: 0,
+        deleteImageId: null,
+        deleteOptionId: deleteOptions,
+        updateOptionList: [...editOptions],
+      };
+
+      formData.append(
+        'patch',
+        new Blob([JSON.stringify(patchDatas)], { type: 'application/json' })
+      );
+      console.log(patchDatas);
+      const res = await api.patch(`/api/items/${getItemId}`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      console.log(res);
+      navigate('/admin/productlist');
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <form className="w-full">
       <div className="w-175">
-        <TitleInput setTitle={setTitle} />
-        <CategoryInput category={category} setCategory={setCategory} />
-        <PriceInput setPrice={setPrice} />
+        <TitleInput
+          datas={datas}
+          pathName={pathName}
+          setTitle={setTitle}
+          setEditTitle={setEditTitle}
+        />
+        <CategoryInput
+          datas={datas}
+          pathName={pathName}
+          setCategory={setCategory}
+          editCategory={editCategory}
+          setEditCategory={setEditCategory}
+        />
+        <PriceInput
+          datas={datas}
+          pathName={pathName}
+          setPrice={setPrice}
+          setEditPrice={setEditPrice}
+        />
       </div>
       <div className="w-200">
         <ImageInput
@@ -84,20 +158,37 @@ export default function ProductForm() {
           setImagePreview={setImagePreview}
           images={images}
           setImages={setImages}
+          editImages={editImages}
+          setEditImages={setEditImages}
         />
         <OrderInput
+          datas={datas}
+          pathName={pathName}
           deliveryPrice={deliveryPrice}
           setDeliveryPrice={setDeliveryPrice}
+          setEditDeliveryPrice={setEditDeliveryPrice}
         />
         <OptionInput
+          datas={datas}
+          pathName={pathName}
           options={options}
           setOptions={setOptions}
           setDefaultCount={setDefaultCount}
+          editOptions={editOptions}
+          setEditOptions={setEditOptions}
+          deleteOptions={deleteOptions}
+          setDeleteOptions={setDeleteOptions}
         />
       </div>
-      <ContentsInput contents={contents} setContents={setContents} />
-      {/* <Button postData={postData} patchData={patchData} /> */}
-      <Button postData={postData} />
+      <ContentsInput
+        datas={datas}
+        pathName={pathName}
+        contents={contents}
+        setContents={setContents}
+        editContents={editContents}
+        setEditContents={setEditContents}
+      />
+      <Button postData={postData} patchData={patchData} />
     </form>
   );
 }
