01f3ed1a6c847b5339cf498634aea449bbd7878c
SigLee <lgh941107@gmail.com>
2023-09-14 11:55:41 +0900

    [feat]: 결제 관련 사전검증 API 구현 - 결제 요청 시 결제 관련 정보를 백엔드 및 결제 서버에 저장한 뒤 해당 정보를 통한 검증

--- BE/backend/src/main/java/project/main/webstore/domain/payment/service/PaymentService.java
+++ BE/backend/src/main/java/project/main/webstore/domain/payment/service/PaymentService.java
@@ -1,21 +1,26 @@
 package project.main.webstore.domain.payment.service;
 
-import com.siot.IamportRestClient.IamportClient;
-import com.siot.IamportRestClient.exception.IamportResponseException;
 import com.siot.IamportRestClient.request.PrepareData;
 import com.siot.IamportRestClient.response.IamportResponse;
-import com.siot.IamportRestClient.response.Prepare;
+import java.math.BigDecimal;
+import java.util.LinkedHashMap;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.client.RestTemplate;
 import project.main.webstore.domain.payment.exception.PaymentExceptionCode;
 import project.main.webstore.exception.BusinessLogicException;
 import project.main.webstore.redis.RedisUtils;
-
-import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
 @Service
@@ -23,129 +28,76 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @RequiredArgsConstructor
 public class PaymentService {
 
-    private final IamportClient client;
     private final RedisUtils redisUtils;
+    private final RestTemplate template = new RestTemplate();
     private final int SAVE_PAY_ACCESS_TIME = 10;
+    private final String IAMPORT_URL = "https://api.iamport.kr";
+    @Value("${iamport.merchant_uid}")
+    private String merchant_uid;
+    @Value("${iamport.api-key}")
+    private String api;
+    @Value("${iamport.secret-key}")
+    private String apiSecret;
 
+    public PrepareData postPrepare(Long amount,String orderNumber) {
+        String accessToken = getAccessToken();
+        BigDecimal totalPrice = BigDecimal.valueOf(amount);
+        PrepareData prepareData = new PrepareData(merchant_uid, totalPrice);
 
-    public PrepareData postPrepare(PrepareData prepareData) {
-        try {
-            IamportResponse<Prepare> iamportResponse = client.postPrepare(prepareData);
-        } catch (IamportResponseException e) {
-            if (e.getHttpStatusCode() == 401) { //검증 실패 ( 토큰 문제)
-                log.error("### 결제 서버 권한 문제 발생 {}", e.getMessage());
-                log.trace("결제 서버 권한 문제 발생 ", e);
-                throw new BusinessLogicException(PaymentExceptionCode.PAYMENT_AUTHENTIC_NOT_FOUND);
-            } else {    //500 에러 발생
-                log.error("### 결제 서버 에러 발생 {}", e.getMessage());
-                log.trace("결제 서버 에러 발생 ", e);
-                throw new BusinessLogicException(PaymentExceptionCode.PAYMENT_SERVER_ERROR);
-            }
-        } catch (IOException e) {
-            log.error("### 연결 시 에러 발생");
-            log.trace("### 에러 발생", e);
-            throw new BusinessLogicException(PaymentExceptionCode.PREPARE_GET_ERROR);
-        } catch (AssertionError e) {
-            log.error("### 가격 정보 불일치");
-            throw new BusinessLogicException(PaymentExceptionCode.PAYMENT_AMOUNT_CONFLICT);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        headers.add("Authorization", "Bearer " + accessToken);
+        HttpEntity<PrepareData> request = new HttpEntity<>(prepareData, headers);
+
+        ResponseEntity<IamportResponse> response = template.postForEntity(
+                IAMPORT_URL + "/payments/prepare", request, IamportResponse.class);
+        HttpStatus statusCode = response.getStatusCode();
+        if (response.getBody().getResponse() == null) {
+            response = template.exchange(IAMPORT_URL + "/payments/prepare", HttpMethod.PUT, request, IamportResponse.class);
         }
-        redisUtils.set(prepareData.getMerchant_uid(),prepareData,SAVE_PAY_ACCESS_TIME);
+        redisUtils.set(orderNumber,amount,SAVE_PAY_ACCESS_TIME);
+
         return prepareData;
-    }
-    //사후 검증
-    public String validatePayment(PrepareData prepareData, String impUid, String orderNumber, long userId)
-    {
-        PrepareData findByKey = (PrepareData) redisUtils.findByKey(prepareData.getMerchant_uid());
-        int redisPrice = findByKey.getAmount().intValue();
-
-        validPaymentAccessTime(findByKey);
-
-        int postPrice = prepareData.getAmount().intValue();
-        int impPrice = 0;
-
-        try {
-            impPrice = client.paymentByImpUid(impUid).getResponse().getAmount().intValue();
-        } catch (IamportResponseException e) {
-            if (e.getHttpStatusCode() == 401) { //검증 실패 ( 토큰 문제)
-                log.error("### 결제 서버 권한 문제 발생 {}", e.getMessage());
-                log.trace("결제 서버 권한 문제 발생 ", e);
-                throw new BusinessLogicException(PaymentExceptionCode.PAYMENT_AUTHENTIC_NOT_FOUND);
-            } else {    //500 에러 발생
-                log.error("### 결제 서버 에러 발생 {}", e.getMessage());
-                log.trace("결제 서버 에러 발생 ", e);
-                throw new BusinessLogicException(PaymentExceptionCode.PAYMENT_SERVER_ERROR);
-            }
-        } catch (IOException e){
-            log.error("### 연결 시 에러 발생");
-            log.trace("### 에러 발생", e);
-            throw new BusinessLogicException(PaymentExceptionCode.PREPARE_GET_ERROR);
-        }
-        validPriceEquals(postPrice,impPrice,redisPrice);
-
-        // 일치하면 주문완료로 상태 변경
-
-        // 결제정보 저장
-
-        // 장바구니에 담겨있던 물건 삭제
-
-        return "결제 성공";
-    }
+}
 
     //사전 검증
-    public IamportResponse<Prepare> getPrepare(String orderNumber, int price) {
-        IamportResponse<Prepare> prepare = null;
-        PrepareData findByKey = (PrepareData) redisUtils.findByKey(orderNumber);
+    public Integer validatePayment(String orderNumber) {
+        String accessToken = getAccessToken();
 
-        validPaymentAccessTime(findByKey);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        headers.add("Authorization","Bearer " + accessToken);
 
-        try {
-            prepare = client.getPrepare(orderNumber);
-            assertEquals(prepare.getCode(), 0);  //0 일치 검증
-        } catch (IamportResponseException e) {
-            if (e.getHttpStatusCode() == 401) { //검증 실패 ( 토큰 문제)
-                log.error("### 결제 서버 권한 문제 발생 {}", e.getMessage());
-                log.trace("결제 서버 권한 문제 발생 ", e);
-                throw new BusinessLogicException(PaymentExceptionCode.PAYMENT_AUTHENTIC_NOT_FOUND);
-            } else {    //500 에러 발생
-                log.error("### 결제 서버 에러 발생 {}", e.getMessage());
-                log.trace("결제 서버 에러 발생 ", e);
-                throw new BusinessLogicException(PaymentExceptionCode.PAYMENT_SERVER_ERROR);
-            }
-        } catch (IOException e) {
-            log.error("### 연결 시 에러 발생");
-            log.trace("### 에러 발생", e);
-            throw new BusinessLogicException(PaymentExceptionCode.PREPARE_GET_ERROR);
+        HttpEntity request = new HttpEntity<>( headers);
+        ResponseEntity<IamportResponse> response = template.exchange(IAMPORT_URL + "/payments/prepare/{merchant_uid}",HttpMethod.GET,request, IamportResponse.class,"imp82370458");
 
-        } catch (AssertionError e) {
-            log.error("### 아임포트 연결 에러 발생  = {}", prepare.getMessage());
-            log.trace("### 에러 발생", e);
-            throw new BusinessLogicException(PaymentExceptionCode.PREPARE_GET_ERROR);
-        }
+        LinkedHashMap responseBody = (LinkedHashMap) response.getBody().getResponse();
+        Integer amount = (Integer) responseBody.get("amount");
+        Integer byKey = (Integer) redisUtils.findByKey(orderNumber);
 
-        validPriceEquals(price, findByKey.getAmount().intValue());
-
-        return prepare;
-    }
-
-    private void validPriceEquals(int requestPrice, int iamPrice, int redisPrice) {
-        if (requestPrice != iamPrice || requestPrice != redisPrice) {
-
-            //환불 로직이 들어가야한다.
-
-
-            throw new BusinessLogicException(PaymentExceptionCode.FABRICATED_PAYMENT);
-        }
-    }
-    private void validPriceEquals(int iamPrice, int checkPrice) {
-        if (checkPrice != iamPrice) {
-            throw new BusinessLogicException(PaymentExceptionCode.PAYMENT_AMOUNT_CONFLICT);
-        }
-    }
-
-    private void validPaymentAccessTime(PrepareData findByKey) {
-        if(findByKey == null){
+        if(byKey == null){
             throw new BusinessLogicException(PaymentExceptionCode.PAYMENT_ACCESS_TIME_ERROR);
         }
+        if(amount != byKey){
+            throw new BusinessLogicException(PaymentExceptionCode.FABRICATED_PAYMENT);
+        }
+        return amount;
     }
 
+    private String getAccessToken() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        LinkedMultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
+        params.put("imp_key", List.of(api));
+        params.put("imp_secret", List.of(apiSecret));
+        HttpEntity request = new HttpEntity<>(params, headers);
+
+        ResponseEntity<IamportResponse> accessToken = template.exchange(
+                IAMPORT_URL + "/users/getToken",
+                HttpMethod.POST, request, IamportResponse.class);
+        LinkedHashMap response = (LinkedHashMap) accessToken.getBody().getResponse();
+        return (String) response.get("access_token");
+    }
 }
