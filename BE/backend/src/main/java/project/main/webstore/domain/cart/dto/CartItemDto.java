096ccfbd4b3df88846a6ecfb5abee8ee50113f67
SigLee <lgh941107@gmail.com>
2023-09-19 12:04:28 +0900

    [docs]: CartItemDto 기본 값 추가

--- BE/backend/src/main/java/project/main/webstore/domain/cart/dto/CartItemDto.java
+++ BE/backend/src/main/java/project/main/webstore/domain/cart/dto/CartItemDto.java
@@ -1,5 +1,6 @@
 package project.main.webstore.domain.cart.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CartItemDto {
+    @Schema(example = "1")
     private Long optionId;
+    @Schema(example = "10")
     private int itemCount;
 }
