81b7905a03ff0ebfa7cca348cd9091879d92ad6a
kimyonghui <kyhui1115@gmail.com>
2023-09-19 17:56:39 +0900

    Revert "[remove] dev/BE 데이터 삭제"
    This reverts commit e4267bad47c4f5416bda8210e694d5617e5a971a.

---
+++ BE/backend/src/test/java/project/main/webstore/WebstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package project.main.webstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
