1a4fb4efa1e4859f86179994ffe8118fa269c952
Siglee2247 <lgh941107@gmail.com>
2023-09-26 17:40:05 +0900

    [test]: TestContainer 적용 - 기존의 사전 작업 제거
    Signed-off-by: Siglee2247 <lgh941107@gmail.com>

--- BE/backend/src/test/java/project/main/webstore/totalTest/ItemControllerTest.java
+++ BE/backend/src/test/java/project/main/webstore/totalTest/ItemControllerTest.java
@@ -2,12 +2,9 @@ package project.main.webstore.totalTest;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import java.io.IOException;
 import java.lang.reflect.Type;
 import java.net.URI;
-import java.net.URISyntaxException;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,23 +15,27 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import project.main.webstore.domain.item.dto.ItemIdResponseDto;
 import project.main.webstore.domain.item.dto.ItemPostDto;
-import project.main.webstore.domain.item.dto.ItemResponseDto;
 import project.main.webstore.domain.item.stub.ItemStub;
 import project.main.webstore.dto.ResponseDto;
+
 
 /*
  * DB에 저장 되어 있는 사용자 ID : 1L 클라이언트 2L 어드민   asdffcx1111
  * 상품 생성은 상품을 생성한 뒤 삭제하는 로직을 통해 구현한다.
  * 상품 수정 및 조회는 기존에 가지고 있는 데이터를 활용해서 구현한다.
- * 저장된 item 2L : 이미지를 가지고 있음, 3L : 이미지가 없음
- * item 2L 에 연결된 옵션들 : 옵션 1L, 옵션 2L
+ * 저장된 item 1L : 이미지를 가지고 있음, 2L : 이미지 있음  [2장]
+ * item 1L 에 연결된 옵션들 : 옵션 1L, 옵션 2L 옵션 3L
  **/
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @AutoConfigureMockMvc
 public class ItemControllerTest {
 
+    @Container
+    static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8");
     final String URL = "http://localhost:";
     final String DEFAULT_URL = "/api/items";
     TestRestTemplate clientLoginTest = new TestRestTemplate("client@gmail.com", "asdffcx1111");
@@ -47,13 +48,6 @@ public class ItemControllerTest {
     @LocalServerPort
     private int port;
 
-    @AfterEach
-    void after() {
-        String url = URL + port + DEFAULT_URL;
-        System.out.println("### DELETE RESULT ItemId = " + itemId);
-        adminLoginTest.delete(url, itemId);
-    }
-
     @Test
     @DisplayName("상품 등록 : 이미지 포함")
     void post_item_test() throws Exception {
@@ -61,7 +55,7 @@ public class ItemControllerTest {
         String content = gson.toJson(post);
 
         HttpEntity<MultiValueMap<String, Object>> requestEntity =
-                itemStub.getMultipartTwoImageAndJsonDataRequest(content);
+                itemStub.getMultipartTwoImageAndJsonDataRequest("post", content);
 
         String url = "http://localhost:" + port + "/api/items";
         System.out.println("url = " + url);
@@ -86,7 +80,7 @@ public class ItemControllerTest {
         String content = gson.toJson(post);
 
         HttpEntity<MultiValueMap<String, Object>> requestEntity =
-                itemStub.getMultipartJsonDataRequest(content);
+                itemStub.getMultipartJsonDataRequest("post", content);
 
         String url = "http://localhost:" + port + "/api/items";
         System.out.println("url = " + url);
@@ -104,40 +98,7 @@ public class ItemControllerTest {
         System.out.println("##### itemId = " + itemId);
     }
 
-    @Test
-    @DisplayName("상품 수정 : 이미지 없음")
-    void patch_item_no_image_test() throws Exception {
-        //상품 조회
 
-    }
-
-    private void requestItemGet() {
-        String url = "http://localhost:" + port + "/api/items/{itemId}";
-        ResponseEntity<String> getResponse = adminLoginTest.getForEntity(url, String.class, itemId);
-        Type responseType = new TypeToken<ResponseDto<ItemResponseDto>>() {
-        }.getType();
-        ResponseDto<ItemResponseDto> getResponseDto = gson.fromJson(getResponse.getBody(),
-                responseType);
-    }
-
-    private void requestItemPost()
-            throws IOException, URISyntaxException {
-        ItemPostDto post = itemStub.createPostDtoWithImage();
-        String content = gson.toJson(post);
-
-        HttpEntity<MultiValueMap<String, Object>> requestEntity =
-                itemStub.getMultipartTwoImageAndJsonDataRequest(content);
-
-        String url = "http://localhost:" + port + "/api/items";
-        System.out.println("url = " + url);
-        ResponseEntity<String> response = adminLoginTest.postForEntity(new URI(url),
-                requestEntity, String.class);
-        String body = response.getBody();
-        Type responseType = new TypeToken<ResponseDto<ItemIdResponseDto>>() {
-        }.getType();
-        ResponseDto<ItemIdResponseDto> responseDto = gson.fromJson(body, responseType);
-        itemId = responseDto.getData().getItemId();
-    }
 
 
 }
