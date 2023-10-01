28391217329e14aafaf00e9ff46386e36960f337
Siglee2247 <lgh941107@gmail.com>
2023-10-01 16:46:37 +0900

    [feat]: CartMapper 기능 수정 - CartMapper에서 변환하도록 수정
    Signed-off-by: Siglee2247 <lgh941107@gmail.com>

--- BE/backend/src/main/java/project/main/webstore/domain/cart/mapper/CartMapper.java
+++ BE/backend/src/main/java/project/main/webstore/domain/cart/mapper/CartMapper.java
@@ -8,10 +8,14 @@ import project.main.webstore.domain.cart.dto.CartGetResponseDto;
 import project.main.webstore.domain.cart.dto.CartIdResponseDto;
 import project.main.webstore.domain.cart.dto.CartItemDto;
 import project.main.webstore.domain.cart.dto.LocalCartDto;
+import project.main.webstore.domain.cart.dto.OptionDto;
 import project.main.webstore.domain.cart.entity.Cart;
+import project.main.webstore.domain.cart.entity.CartItem;
+import project.main.webstore.domain.image.dto.ImageDto;
+import project.main.webstore.domain.image.mapper.ImageMapper;
 
 @Component
-public class CartMapper {
+public class CartMapper extends ImageMapper {
 
     public LocalCartDto toLocal(CartItemDto post){
         return new LocalCartDto(post.getOptionId(),post.getItemCount());
@@ -26,9 +30,40 @@ public class CartMapper {
     }
 
     public CartGetResponseDto toGetResponse(Cart result) {
-        if (result != null)
-            return new CartGetResponseDto(result);
-        return null;
+        if(result == null)
+            return null;
+        return CartGetResponseDto.builder()
+                .cartId(result.getId())
+                .userId(result.getId())
+                .itemList(this.getOptionDtoList(result.getCartItemList()))
+                .deliveryPrice(result.getDeliveryPrice())
+                .totalPrice(result.getOriginalTotalPrice())
+                .discountedPrice(result.getDiscountedTotalPrice())
+                .build();
+    }
+    private List<OptionDto> getOptionDtoList(List<CartItem> cartItemList){
+        return cartItemList != null ? cartItemList.stream().map(this::getOptionDto).collect(Collectors.toList()) : new ArrayList<>();
+    }
+
+    private OptionDto getOptionDto(CartItem cartItem){
+        return OptionDto.builder()
+                .itemId(cartItem.getOption().getItem().getItemId())
+                .itemName(cartItem.getOption().getItem().getItemName())
+                .optionId(cartItem.getOption().getOptionId())
+                .count(cartItem.getItemCount())
+                .defaultPrice(cartItem.getOption().getItem().getItemPrice())
+                .additionalPrice(cartItem.getOption().getAdditionalPrice())
+                .discountRate(cartItem.getOption().getItem().getDiscountRate())
+                .optionName(cartItem.getOption().getOptionName())
+                .optionDetail(cartItem.getOption().getOptionDetail())
+                .imageInfo(getImageDto(cartItem))
+                .build();
+
+    }
+
+    private ImageDto getImageDto(CartItem cartItem) {
+        return cartItem.getOption().getItem().getDefaultImage() != null ? toResponseDto(
+                cartItem.getOption().getItem().getDefaultImage()) : null;
     }
 
     public List<Long> toList(List<Long> idList) {
