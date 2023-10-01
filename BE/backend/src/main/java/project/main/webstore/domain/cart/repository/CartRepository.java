afe09fb514bc6ac4dc6cb6c82b3a6e6a1915ae8f
Siglee2247 <lgh941107@gmail.com>
2023-10-01 20:16:25 +0900

    [refactor]: cartService patch 메서드 메서드 분리 및 CartRepository 제거 - patch 메서드 역할 과다로 인한 메서드 분리 - cartRepository 미사용으로 인한 제거
    Signed-off-by: Siglee2247 <lgh941107@gmail.com>

--- BE/backend/src/main/java/project/main/webstore/domain/cart/repository/CartRepository.java
+++
@@ -1,7 +0,0 @@
-package project.main.webstore.domain.cart.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import project.main.webstore.domain.cart.entity.Cart;
-
-public interface CartRepository extends JpaRepository<Cart,Long> {
-}
