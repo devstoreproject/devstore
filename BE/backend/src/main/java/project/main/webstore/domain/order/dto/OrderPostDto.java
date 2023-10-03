c136605e247b5f651a8c785d098de546706f79b2
Siglee2247 <lgh941107@gmail.com>
2023-10-03 14:40:22 +0900

    [fit]: order post dto 스웨거 수정 - 스웨거 상으로는 userId가 출력되지 않도록 수정
    Signed-off-by: Siglee2247 <lgh941107@gmail.com>

--- BE/backend/src/main/java/project/main/webstore/domain/order/dto/OrderPostDto.java
+++ BE/backend/src/main/java/project/main/webstore/domain/order/dto/OrderPostDto.java
@@ -1,5 +1,6 @@
 package project.main.webstore.domain.order.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -16,6 +17,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class OrderPostDto {
     @Setter
+    @Schema(hidden = true)
     private Long userId;
     @Pattern(regexp = "^[가-힣a-zA-Z0-9\\s]*$", message = "특수문자 제외 20자 이내로 작성하세요")
     @Size(min = 0, max = 20)
