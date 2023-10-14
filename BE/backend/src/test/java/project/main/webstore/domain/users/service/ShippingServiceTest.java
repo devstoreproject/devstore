c5a51c1239f251c9dbd7e1c011302c4c753c292c
Siglee2247 <lgh941107@gmail.com>
2023-10-14 14:57:37 +0900

    [test]: 배송지 스장 서비스 테스트 - 배송지 수정 단위 테스트
    Signed-off-by: Siglee2247 <lgh941107@gmail.com>

--- BE/backend/src/test/java/project/main/webstore/domain/users/service/ShippingServiceTest.java
+++ BE/backend/src/test/java/project/main/webstore/domain/users/service/ShippingServiceTest.java
@@ -1,5 +1,6 @@
 package project.main.webstore.domain.users.service;
 
+import java.util.Optional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,9 @@ import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import project.main.webstore.domain.order.exception.OrderExceptionCode;
 import project.main.webstore.domain.users.entity.ShippingInfo;
+import project.main.webstore.domain.users.entity.User;
 import project.main.webstore.domain.users.repository.ShippingRepository;
 import project.main.webstore.domain.users.stub.UserStub;
 
@@ -35,6 +38,46 @@ class ShippingServiceTest {
         // then
         Assertions.assertThat(result.getAddress()).usingRecursiveComparison().isEqualTo(request.getAddress());
     }
+
+    @Test
+    @DisplayName("배송 주소 등록 테스트 : 성공")
+    void patch_ship_address_test() throws Exception{
+        // given
+        ShippingInfo request = userStub.createShippingInfo();
+
+        User savedUser = userStub.createUser(1L);
+        ShippingInfo savedShipInfo = userStub.createShippingInfo();
+
+
+        BDDMockito.given(userValidService.validUser(ArgumentMatchers.anyLong())).willReturn(savedUser);
+        BDDMockito.given(shippingRepository.findByInfoId(ArgumentMatchers.anyLong())).willReturn(
+                Optional.of(savedShipInfo));
+        // when
+        ShippingInfo result = service.updateAddressInfo(request, 1L);
+        // then
+        Assertions.assertThat(result.getAddress()).usingRecursiveComparison().isEqualTo(request.getAddress());
+        Assertions.assertThat(result.getRecipient()).isEqualTo(request.getRecipient());
+    }
+
+    @Test
+    @DisplayName("배송 주소 등록 테스트 : 실패")
+    void patch_ship_address_fail_test() throws Exception{
+        // given
+        ShippingInfo request = userStub.createShippingInfo();
+
+        User savedUser = userStub.createUser(1L);
+        ShippingInfo savedShipInfo = userStub.createShippingInfo();
+
+
+        BDDMockito.given(userValidService.validUser(ArgumentMatchers.anyLong())).willReturn(savedUser);
+        BDDMockito.given(shippingRepository.findByInfoId(ArgumentMatchers.anyLong())).willReturn(
+                Optional.empty());
+        // when
+        // then
+        Assertions.assertThatThrownBy(()-> service.updateAddressInfo(request, 1L)).hasMessage(
+                OrderExceptionCode.SHIPPING_INFO_NOT_FOUND.getMessage());
+    }
+
 
 
 }
