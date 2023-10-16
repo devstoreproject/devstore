825c7d507202ad6a31896b108e00b9337d7bd315
Siglee2247 <lgh941107@gmail.com>
2023-10-16 18:35:46 +0900

    [test]: CheckLoginUser admin 여부 테스트
    Signed-off-by: Siglee2247 <lgh941107@gmail.com>

---
+++ BE/backend/src/test/java/project/main/webstore/utils/CheckLoginUserTest.java
@@ -0,0 +1,35 @@
+package project.main.webstore.utils;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import project.main.webstore.domain.users.enums.UserRole;
+import project.main.webstore.domain.users.exception.UserExceptionCode;
+import project.main.webstore.security.dto.UserInfoDto;
+
+class CheckLoginUserTest {
+
+    @Test
+    @DisplayName("로그인 회원 관리자 여부 체크 : 관리자 아님")
+    void login_admin_test() throws Exception {
+        // given
+        String principal = "anonymousUser";
+        // when
+        // then
+        Assertions.assertThatThrownBy(() -> CheckLoginUser.validAdmin(principal))
+                .hasMessage(UserExceptionCode.USER_NOT_LOGIN.getMessage());
+    }
+
+    @Test
+    @DisplayName("로그인 회원 관리자 여부 체크")
+    void login_admin_no_admin_test() throws Exception {
+        // given
+        UserInfoDto principal = new UserInfoDto("1", "client@gmail.com", "김복자", UserRole.CLIENT);
+        // when
+        // then
+        Assertions.assertThatThrownBy(() -> CheckLoginUser.validAdmin(principal))
+                .hasMessage(UserExceptionCode.NOT_ADMIN.getMessage());
+    }
+
+
+}
