442422077ef94667074aa89a8e0616116ee8312c
Siglee2247 <lgh941107@gmail.com>
2023-10-17 17:15:30 +0900

    [test]: 옵션 수정 서비스 테스트 - 옵션 누락된 코드 추가 - 수정 성공 및 실패 테스트 추가
    Signed-off-by: Siglee2247 <lgh941107@gmail.com>

--- BE/backend/src/test/java/project/main/webstore/domain/item/service/OptionServiceTest.java
+++ BE/backend/src/test/java/project/main/webstore/domain/item/service/OptionServiceTest.java
@@ -1,10 +1,12 @@
 package project.main.webstore.domain.item.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 
-import org.assertj.core.api.Assertions;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import project.main.webstore.domain.item.entity.ItemOption;
 import project.main.webstore.domain.item.repository.OptionRepository;
 import project.main.webstore.domain.item.stub.ItemStub;
+import project.main.webstore.domain.order.exception.OrderExceptionCode;
 
 @ExtendWith(MockitoExtension.class)
 class OptionServiceTest {
@@ -37,7 +40,36 @@ class OptionServiceTest {
         // when
         ItemOption result = service.writeOption(post, 1L);
         // then
-        Assertions.assertThat(result).usingRecursiveComparison().isEqualTo(afterRepoMockEntity);
+        assertThat(result).usingRecursiveComparison().isEqualTo(afterRepoMockEntity);
+    }
+
+    @Test
+    @DisplayName("옵션 등록 테스트 : 성공")
+    void patch_option_test() throws Exception{
+        // given
+        ItemOption afterRepoMockEntity = itemStub.createItemOption(1L);
+        ItemOption patch = itemStub.createItemOption(1L);
+        patch.setOptionName("수정할이름");
+        patch.setOptionDetail("수정할디테일");
+        given(optionRepository.findById(anyLong())).willReturn(Optional.of(afterRepoMockEntity));
+        // when
+        ItemOption result = service.editOption(patch);
+        // then
+        assertThat(result.getOptionName()).isEqualTo(patch.getOptionName());
+        assertThat(result.getOptionDetail()).isEqualTo(patch.getOptionDetail());
+    }
+
+    @Test
+    @DisplayName("옵션 등록 테스트 : 실패[조회 불가]")
+    void patch_option_fail_test() throws Exception{
+        // given
+        ItemOption patch = itemStub.createItemOption(1L);
+        patch.setOptionName("수정할이름");
+        patch.setOptionDetail("수정할디테일");
+        given(optionRepository.findById(anyLong())).willReturn(Optional.empty());
+        // when
+        // then
+        assertThatThrownBy(()-> service.editOption(patch)).hasMessage(OrderExceptionCode.OPTION_NOT_FOUND.getMessage());
     }
 
 
